50309c36a2f227e8c4ead7ffe4ded10c95f05ac0
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-08-31 06:31:46 +0200

    Time: 582 ms (14.29%), Space: 75.1 MB (14.29%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.js
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.js
@@ -7,7 +7,7 @@ var minOperations = function(nums) {
   const set = [...new Set(nums)].sort((a, b) => a - b)
     let min = nums.length
     for(let i = 0; i < set.length; i++){
-        let end  = bs(set, set[i] + nums.length-1) 
+        let end  = bs(set, i ,set[i] + nums.length-1) 
         let curr = nums.length  - (end - i  + 1)
         min = Math.min(curr, min) 
         
@@ -17,8 +17,8 @@ var minOperations = function(nums) {
     
 };
 
-function bs(arr, target){
-    let l = 0
+function bs(arr,start ,  target){
+    let l = start
     let r = arr.length -1
     while(l < r){
      let m = Math.ceil((l+r) / 2)
