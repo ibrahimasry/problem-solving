0460b6720d9da4d9d50061e396dc135257384ca4
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-06 11:54:59 +0200

    Time: 998 ms (71.43%), Space: 67.5 MB (71.43%) - LeetHub

--- 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.js
+++ 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.js
@@ -2,21 +2,37 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var gcdSort = function(nums) {
-    
-    const numsSet = new Set(nums)
-    const  numsSorted = [...nums].sort((a, b) => a - b)
-    let max = numsSorted[numsSorted.length - 1] + 1
-    let p = 2
-    const parent = nums.reduce((a, b) => {
+
+class UnionFind {
+    constructor(arr){
+        this.parent = arr.reduce((a, b) => {
           a[b] = b
           return a
     }, {})
+    }
+     find = (x) => {
+        if(this.parent[x] !== x) 
+         this.parent[x] = this.find(this.parent[x])
+        return this.parent[x]
+    }
+
+   union = (a, b) =>{
+        let pa = this.find(a)
+        let pb = this.find(b)
+        if(pa !== pb)
+            this.parent[pb] = pa
+   }
+     
+
+}
+
+function seive(union, max, numsSet){
+    let p = 2
     const primes = Array(max + 1).fill(true)
     while (p < max){
         let prev = 0
         if(primes[p] ) {
-        for(let i = p ; i < max; i+=p){
+        for(let i = p ; i < max; i+= p){
             if(numsSet.has(i)){
                if(prev > 0)
                     union(i, prev)
@@ -27,20 +43,18 @@ var gcdSort = function(nums) {
         }
         p++
     }
+
+}
+var gcdSort = function(nums) {
+    
+    const  numsSet = new Set(nums)
+    const  numsSorted = [...nums].sort((a, b) => a - b)
+    let    max = numsSorted[numsSorted.length - 1] + 1
+    const {union, find} = new UnionFind(numsSorted)
+    seive(union, max, numsSet)
     for(let  i = 0; i< nums.length; i++)
-        if(nums[i] != numsSorted[i] && find(nums[i]) !== find(numsSorted[i]))
+        if(find(nums[i]) !== find(numsSorted[i]))
             return false
     
-    function union(a, b){
-        let pa = find(a)
-        let pb = find(b)
-        if(pa !== pb)
-            parent[pb] = pa
-    }
-    function find(x){
-        if(parent[x] !== x) 
-         parent[x] = find(parent[x])
-        return parent[x]
-    }
     return true
 };
