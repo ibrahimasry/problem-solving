e0955ddd352647dcf675a8d66677b928490e4476
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-07 17:53:46 +0200

    Time: 843 ms (37.50%), Space: 69.1 MB (100.00%) - LeetHub

---
+++ 952-largest-component-size-by-common-factor/952-largest-component-size-by-common-factor.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+class UnionFind {
+    constructor(arr){
+        this.parent = arr.reduce((a, b) => {
+          a[b] = b
+          return a
+    }, {})
+        this.sizes = Array(Math.max(...arr) + 1).fill(1)
+    }
+     find = (x) => {
+        if(this.parent[x] !== x) 
+         this.parent[x] = this.find(this.parent[x])
+        return this.parent[x]
+    }
+
+   union = (a, b) =>{
+        let pa = this.find(a)
+        let pb = this.find(b)
+        if(pa !== pb){
+            this.parent[pb] = pa
+            this.sizes[pa] += this.sizes[pb]
+        }
+   }
+     
+
+}
+
+function seive(union, max, numsSet){
+    let p = 2
+    const primes = Array(max + 1).fill(true)
+    while (p < max){
+        let prev = 0
+        if(primes[p] ) {
+        for(let i = p ; i < max; i+= p){
+            if(numsSet.has(i)){
+               if(prev > 0)
+                    union(i, prev)
+                prev = i
+            }
+            primes[i] = false
+        }
+        }
+        p++
+    }
+
+}
+var largestComponentSize = function(nums) {
+    
+    const  numsSet = new Set(nums)
+    const  numsSorted = [...nums].sort((a, b) => a - b)
+    let    max = numsSorted[numsSorted.length - 1] + 1
+    const unionFind = new UnionFind(numsSorted)
+    const {union, find} = unionFind
+    seive(union, max, numsSet)
+    return Math.max(...unionFind.sizes) 
+};
