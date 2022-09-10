a1f54fc35805d0d918f37c9c663ca2775ed816f6
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-10 21:03:58 +0200

    Time: 594 ms (50.00%), Space: 68.8 MB (50.00%) - LeetHub

--- 1994-the-number-of-good-subsets/1994-the-number-of-good-subsets.js
+++ 1994-the-number-of-good-subsets/1994-the-number-of-good-subsets.js
@@ -1,29 +1,70 @@
-class Solution:
-    def numberOfGoodSubsets(self, nums: List[int]) -> int:
-        primes = []
-        for i in range(2, 31):
-            isPrimes = True
-            for j in range(2, i):
-                if i % j == 0 :
-                    isPrimes = False
-                    break
-            if isPrimes:
-                primes.append(i)
-                
-        counts = Counter(nums)
-        nums = list(counts.keys())
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfGoodSubsets = function(nums) {
+    
+    const primes = []
+    
+    for(let i = 2; i < 31; i++)
+ {
+
+     let isPrimes = true
+     for(let j = 2; j < i; j++){
+            if(i % j == 0)
+                {
+                    isPrimes = false
+                }
+        }
+     if(isPrimes)
+         primes.push(i)
+}
+    const count = {}
+    for(let c of nums){
+        count[c] = count[c] || 0
+        count[c]++
+    }
+    nums = Object.keys(count).map((a) => +a)
+    const mod = 10 ** 9 + 7
+    let ones = BigInt(1)
+    for (let i = 0; i < count[1] || 0; i++) {
+      ones *= 2n;
+      ones %= BigInt(mod);
+    }
+    const ans = ( BigInt((dfs(nums, primes, count ,  0 , 0 , {}) % mod - 1) )) * ones
+    return ans % BigInt(mod)
+      
+};
+
+
+function dfs(nums, primes, count, i, mask, cache){
+    if(i == nums.length)
+        return 1
+    let ans = 0 
+    let hash = mask + "-" + i
+    if(cache[hash] !== undefined)
+        return cache[hash]
+    ans += dfs(nums, primes, count , i + 1,  mask , cache)
+    let key = nums[i]
+    if(key != 1){
+        let curr = 0
+        for(let j = 0; j < primes.length; j++){
+            let p = primes[j]
+            if(key % p === 0)
+                {
+                    curr |= (1 << j)
+                }
+        }
+        if(key % 4 !== 0 && key % 9 !== 0 && key % 25 !== 0 && ((mask & curr) === 0)){
+            ans += (dfs(nums, primes, count, i+1, mask | curr, cache)) * count[key] 
+
+        } 
         
-        @lru_cache(None)
-        def dfs(i, mask):
-            if i == len(nums):
-                return 1
-            ans = 0
-            ans = dfs(i + 1, mask)
-            key = nums[i]
-            if key != 1 :
-                isPrimeMask = sum(1 << j for j, p in enumerate(primes) if key % p == 0)
-                if key % 4 != 0 and key % 9 != 0 and key % 25 != 0 and mask & isPrimeMask == 0 :
-                    ans += dfs(i + 1 , mask | isPrimeMask) * counts[key]
-            return ans        
-        mod = 10 ** 9 + 7
-        return ((dfs(0, 0) - 1) * pow(2, counts[1], mod)) % mod
+        
+        
+        
+    }
+            cache[hash]  = ans % 1000000007
+           return cache[hash]
+    
+}
