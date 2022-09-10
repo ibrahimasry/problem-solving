f734dd56af72d4fcaa0410d1541ac58cabd7d7b0
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-10 18:29:10 +0200

    Time: 453 ms (12.50%), Space: 49.2 MB (12.50%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.js
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.js
@@ -7,12 +7,11 @@ var distinctSubseqII = function(s) {
     const dp = {}
     const mod =( 10 ** 9) + 7
     
-    for(let i = 0; i< s.length; i++){
-        let count = Object.values(dp).reduce((a, b)=> a  + b, 0) % mod
-         dp[s[i]] = count + 1
+    for(let i = 0; i < s.length; i++){
+        dp[s[i]]  = Object.values(dp).reduce((a, b)=> a  + b, 0) % mod + 1
         
     }
     
-    return (Object.values(dp).reduce((a, b)=> a  + b , 0) ) % mod
+    return (Object.values(dp).reduce((a, b) => a  + b , 0) ) % mod
     
 };
