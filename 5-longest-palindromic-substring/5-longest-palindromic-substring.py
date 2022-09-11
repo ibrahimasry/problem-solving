0304c781ce1adc1bb9aeba7651ff81565213ced9
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-11 21:09:15 +0200

    Time: 220 ms (95.28%), Space: 13.8 MB (90.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -4,20 +4,18 @@ class Solution:
         r = 0
         c = 0
         dp = [0] * len(m)
-        start = -1
-        end = -1
+        rm = -1
+        cm = -1
         for i in range(1, len(m) - 1) :
             mirror = c * 2 - i
             if i < r :
                 dp[i] = min(dp[mirror], r - i)
             while  m[i +  dp[i]  + 1] == m[i - dp[i] - 1]:
                 dp[i] += 1
-            if dp[i] * 2 > (end - start)  + 1 :
-                start = i - dp[i] 
-                end = i + dp[i] 
-                
                 
             if i + dp[i] > r :
                 c = i
                 r = i + dp[i]
-        return "".join(list(filter(lambda x: x not in ["@", "#", "$"] ,    m[start:end+1])))   
+            if dp[i] > rm :
+                rm , cm = dp[i], i
+        return  s[(cm-rm)//2: (cm+rm)//2]
