f933ca763c31a7de2e5857a080901107a1f042ff
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-11 16:02:41 +0200

    Time: 4681 ms (74.00%), Space: 723.8 MB (6.00%) - LeetHub

--- 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.py
+++ 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.py
@@ -2,6 +2,13 @@ class Solution:
     def numberOfCombinations(self, num: str) -> int:
         n = len(num)
         prefix = [[0] * (n+1) for _ in range(n+1)]
+        lcs = [[0] * (n + 1) for _ in range(n)]
+        
+        
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n) :
+                if num[i] == num[j]:
+                    lcs[i][j] = lcs[i+1][j+1] + 1
         for i in range(n):
             for l in range(1, i+2):
                 j = i - l + 1
@@ -13,10 +20,11 @@ class Solution:
                 elif j < l :
                     curr = prefix[j-1][j]
                 else :
-                        if num[j-l:j] <= num[j:i + 1]:
-                            curr = prefix[j-1][l]
-                        else:
-                            curr = prefix[j-1][l-1]
+                    nextL = lcs[j-l][j]
+                    if nextL >= l or num[j-l+nextL] < num[j+nextL] :
+                        curr = prefix[j-1][l]
+                    else:
+                        curr = prefix[j-1][l-1]
                 prefix[i][l] += prefix[i][l-1] + curr            
         return prefix[n-1][n]  % (10 ** 9 + 7 )              
                     
