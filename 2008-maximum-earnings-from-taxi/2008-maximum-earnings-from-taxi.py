94bd0a15717957d547743cc8189a3a4118782921
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-12 14:08:38 +0200

    Time: 2143 ms (87.89%), Space: 33.3 MB (89.12%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -1,22 +1,30 @@
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
-        
-        
-        
-        events = []
-        
-        for s, e , t in rides :
-            events.append((s, 1,  t , e))
+        """    
+            lru_cache(None)
+            def dfs(i) :
+                if i == len(rides) :
+                    return 0
+                ans1 = dfs(i+1)
+                nextIndex = bisect_left(rides, [rides[i][1],0, 0])
+                s, e, t = rides[i]
+                ans2 = e-s+t+ dfs(nextIndex)
+                return max(ans1, ans2)
+            rides.sort()
+            return dfs(0)
+         """
+        dp = [0] * (n+1)
+        rides.sort()
+        j = 0
+        for i in range(1, n+1) :
+            dp[i] = max(dp[i], dp[i-1])
             
-            
-        res = 0
-        heapq.heapify(events)
-        while len(events):
-            s, time, earning , e= heapq.heappop(events)
-            
-            if time == 1 :
-                heapq.heappush(events, (e , -1 , res + (e-s+earning), s))
-            else :
-                res = max(res, earning)
+            while j < len(rides) and rides[j][0] == i :
+                dp[rides[j][1]] = max(dp[rides[j][1]], dp[i] + rides[j][1] - rides[j][0] + rides[j][2])
+                j += 1
+        return dp[n]        
                 
-        return res         
+        
+        
+        
+    
