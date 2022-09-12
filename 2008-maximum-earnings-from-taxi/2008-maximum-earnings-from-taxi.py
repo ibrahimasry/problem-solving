3c5f7a67b41d738c33cfd19ab6afea0c5b5995b7
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-12 14:12:07 +0200

    Time: 5392 ms (5.15%), Space: 34 MB (50.92%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -12,6 +12,25 @@ class Solution:
                 return max(ans1, ans2)
             rides.sort()
             return dfs(0)
+            
+            //////
+            events = []
+
+            for s, e , t in rides :
+                events.append((s, 1,  t , e))
+
+
+            res = 0
+            heapq.heapify(events)
+            while len(events):
+                s, time, earning , e= heapq.heappop(events)
+
+                if time == 1 :
+                    heapq.heappush(events, (e , -1 , res + (e-s+earning), s))
+                else :
+                    res = max(res, earning)
+
+            return res         
          """
         dp = [0] * (n+1)
         rides.sort()
