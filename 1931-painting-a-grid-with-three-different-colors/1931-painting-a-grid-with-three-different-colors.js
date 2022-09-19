2de3922181d0ae43348045efd2a06e25578f55c0
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-19 02:53:21 +0200

    Time: 4326 ms (50.00%), Space: 168.1 MB (50.00%) - LeetHub

---
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var colorTheGrid = function(m, n) {
+    
+        const  getColor = (col , mask) => ((mask >> (col * 2)) & 3)
+        const  setColor = (color, col, mask) => {
+            const oldColor = getColor(col , mask)
+            return   ((mask ^ ( oldColor << (col* 2))) | (color << (col * 2)))
+        }
+      const mod = BigInt(10 ** 9 + 7)
+
+      const  dfs = (i, lastRow, cache) => { 
+            if( i == m * n)
+                return 1n
+            const key = lastRow + "$" + i
+            if(cache[key] !== undefined) return cache[key]
+            let row = Math.floor(i / m)
+            let col = i % m
+            const  neighbours = new Set()
+            if (row > 0)
+                neighbours.add(getColor(col , lastRow))
+            if (col > 0 )
+                neighbours.add(getColor(col-1 , lastRow))
+            let  res = 0n
+            for (let color of [0, 1, 2])
+                if (!neighbours.has(color)){
+                    res +=  (dfs(i+1, setColor(color, col, lastRow) , cache))
+                }
+            cache[key] = res 
+            return res
+      }
+        
+        return dfs(0, 0 , {})  % mod
+    
+};
