740c142c7d0967d2ba7b178566a443a544821e10
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-21 02:06:46 +0200

    Time: 7549 ms (5.26%), Space: 13.9 MB (71.05%) - LeetHub

--- 1900-the-earliest-and-latest-rounds-where-players-compete/1900-the-earliest-and-latest-rounds-where-players-compete.py
+++ 1900-the-earliest-and-latest-rounds-where-players-compete/1900-the-earliest-and-latest-rounds-where-players-compete.py
@@ -2,21 +2,25 @@ class Solution:
     def earliestAndLatest(self, n, F, S):
         ans = set()
         lru_cache(None)
-        def dfs(pos, i):
+        def dfs(pos):
             M, pairs = len(pos), []
             if M < 2: return
 
             for j in range(M//2):
                 a, b = pos[j], pos[-1-j]
                 if (a, b) == (F, S):
-                    ans.add(i)
-                    return
+                    return [1,1]
                 if a != F and b != F and a != S and b != S:
                     pairs.append((a, b))
             addon = (F, S) if M%2 == 0 else tuple(set([F, S, pos[M//2]]))
+            minx =  sys.maxsize
+            maxx = -sys.maxsize
             for elem in product(*pairs):
-                dfs(sorted(elem + addon), i + 1)
+                result =  dfs(sorted(elem + addon))
+                minx = min(result[0], minx)
+                maxx = max(result[1], maxx)
+            return [minx + 1, maxx + 1]    
 
-        dfs(tuple(range(1, n+1)), 1)
-        return [min(ans), max(ans)]
+
+        return dfs(tuple(range(1, n+1)))
         
