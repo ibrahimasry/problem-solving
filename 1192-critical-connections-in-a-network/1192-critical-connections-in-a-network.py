29c3ad2cb8946768aa0e5532bbf21409f31ff6e6
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-29 10:51:09 +0200

    Time: 5092 ms (25.86%), Space: 73.6 MB (78.76%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -6,23 +6,22 @@ class Solution:
             graph[v].append(u)
             
         ans = []
-        labels = [-1] * n
-        curr = [-1] * n
+        lows = [-1] * n
+        ids = [-1] * n
         def dfs(node, parent, label):
-            if labels[node] != -1:
-                return labels[node]
-            labels[node] = label + 1
-            curr[node] = label + 1
+            if lows[node] != -1:
+                return lows[node]
+            lows[node] = ids[node] = label + 1
             
             for next in graph[node]:
                 if next == parent:
                     continue
-                if labels[next] == -1 :
-                    labels[node] = min(labels[node], dfs(next, node, label+1))
-                    if curr[node] < labels[next]:
+                if lows[next] == -1 :
+                    lows[node] = min(lows[node], dfs(next, node, label+1))
+                    if ids[node] < lows[next]:
                         ans.append([node, next])
                 else:
-                    labels[node] = min(labels[node], labels[next])
-            return labels[node]        
+                    lows[node] = min(lows[node], lows[next])
+            return lows[node]        
         dfs(0, -1, 0)
         return ans
