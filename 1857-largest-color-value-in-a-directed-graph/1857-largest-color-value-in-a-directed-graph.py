9c361a9b11c0402550cd659b1679ce0212d1fbfe
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-29 07:01:44 +0200

    Time: 7702 ms (5.71%), Space: 88.4 MB (65.24%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,29 @@
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        
+        graph = defaultdict(list)
+        indgrees = defaultdict(int)
+        n = len(colors)
+
+        dp = [[0] * 26 for _ in range(n)]
+
+        for u, v in edges:
+            graph[u].append(v)
+            indgrees[v] += 1
+        queue = []
+        colorsVal = [ord(c) - ord('a') for c in colors]
+        for  node in range(n): 
+            if node not in indgrees :
+                queue.append(node)
+                dp[node][(ord(colors[node]) - ord("a"))] = 1
+        visited = 0
+        while len(queue) > 0:
+            curr = queue.pop()
+            visited += 1
+            for next in graph[curr]:
+                indgrees[next] -= 1
+                if indgrees[next] == 0 :
+                    queue.append(next)
+                for n in range(26):
+                    dp[next][n] = max(dp[next][n] , dp[curr][n] + (n == colorsVal[next]))
+        return -1 if visited < len(colors) else  max(max(curr) for curr in dp)            
