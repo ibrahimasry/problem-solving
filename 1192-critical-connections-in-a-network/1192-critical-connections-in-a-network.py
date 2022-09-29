a8d31ac9a81dfcf9bfdcbe84815830e4bf27e649
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-09-29 10:48:33 +0200

    Time: 5184 ms (23.25%), Space: 73.7 MB (78.15%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,26 +1,28 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         graph = defaultdict(list)
-        ans = set()
         for u, v in connections:
             graph[u].append(v)
             graph[v].append(u)
-            ans.add((min(u,v), max(u,v)))
             
+        ans = []
         labels = [-1] * n
+        curr = [-1] * n
         def dfs(node, parent, label):
             if labels[node] != -1:
                 return labels[node]
             labels[node] = label + 1
-            minLabel = label + 1
+            curr[node] = label + 1
+            
             for next in graph[node]:
                 if next == parent:
                     continue
-
-                recursiveVal = dfs(next, node, labels[node])
-                if labels[node] >= recursiveVal:
-                    ans.remove((min(node,next), max(node, next)))
-                minLabel = min(minLabel, recursiveVal)    
-            return minLabel
+                if labels[next] == -1 :
+                    labels[node] = min(labels[node], dfs(next, node, label+1))
+                    if curr[node] < labels[next]:
+                        ans.append([node, next])
+                else:
+                    labels[node] = min(labels[node], labels[next])
+            return labels[node]        
         dfs(0, -1, 0)
-        return [[u,v] for u,v in ans]
+        return ans
