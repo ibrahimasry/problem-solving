90b18d3052b6aaa4172cc7c1a08c53bdeddd406e
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-10-23 23:38:57 +0200

    Time: 3598 ms (76.19%), Space: 24.1 MB (51.19%) - LeetHub

---
+++ 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.py
@@ -0,0 +1,29 @@
+class Solution:
+    def hitBricks(self, grid: List[List[int]], hits: List[List[int]]) -> List[int]:
+        
+        def dfs(i, j):
+            if not (0 <= i < n and 0 <= j < m) :
+                return 0
+
+            if grid[i][j] != 1:
+                return 0
+            res = 1
+            grid[i][j] = 2
+            res += sum(dfs(x, y) for x, y in [[i+1,j], [i-1,j], [i,j+1], [i, j-1]])
+            return res
+        for i, j in hits:
+            grid[i][j] -= 1
+        ans = [0] * len(hits)
+        def connected(i, j):
+            return i == 0 or any([ 0<=x<n and 0<=y< m and grid[x][y] == 2 for x, y in [[i+1,j], [i-1,j], [i,j+1], [i, j-1]]])
+        n = len(grid)
+        m = len(grid[0])
+        for j in range(m):
+            dfs(0, j)
+
+        for h in range(len(hits)-1, -1, -1):
+            i, j = hits[h]
+            grid[i][j] += 1
+            if grid[i][j] == 1 and connected(i, j):
+                ans[h] = (dfs(i, j) - 1)
+        return ans
