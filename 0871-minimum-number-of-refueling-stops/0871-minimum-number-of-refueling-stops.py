5110f86ea2370a60d47d86b6643cff37cc9fdb1b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-10-24 09:57:09 +0200

    Time: 258 ms (60.20%), Space: 13.9 MB (100.00%) - LeetHub

---
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        
+        if len(stations) == 0 :
+            if  startFuel - target >= 0 :
+                return 0
+            else :
+                return -1
+        curr = startFuel
+        pq = []
+        i = 0
+        n = len(stations)
+        ans = 0
+        while curr < target:
+            while i < n and curr >= stations[i][0]:
+                heapq.heappush(pq, -stations[i][1])
+                i += 1
+            if not pq:
+                return -1
+
+            curr += -heapq.heappop(pq)
+            ans += 1
+
+
+
+        return ans
