7a66a74a8040f3741674d613ebde48c0e82b877a
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-10-30 18:58:02 +0200

    Time: 99 ms (86.99%), Space: 15.3 MB (5.69%) - LeetHub

--- 0517-super-washing-machines/0517-super-washing-machines.py
+++ 0517-super-washing-machines/0517-super-washing-machines.py
@@ -5,19 +5,27 @@ class Solution:
         if total % n != 0 :
             return -1
         
-        moves  = [0] * n
-        res = 0
-        avg = total / n
-        for i in range(n - 1):
-            delta = nums[i] - avg
-            nums[i+1] += delta
-            
-            if delta < 0 :
+        left  = [0] * n
+        right = [0] * n
+        ans = 0
+        avg = total // n
+        for i in range(n):
+            if i == 0 :
+                left[i] = 0
+                right[i] = nums[i] - avg
+            elif i == n -1:
+                right[i] = 0
+                left[i] = nums[i] - avg
+            else:
+                left[i] = -right[i-1]
+                right[i] = nums[i] - avg - left[i]
+        for i in range(n):
+            temp = 0
+            if left[i] > 0 :
+                temp+=left[i]
+            if right[i] > 0:
+                temp+=right[i]
+            ans = max(ans, temp)
                 
-                moves[i+1] += -delta
-            else :
-                moves[i] += delta
-            res = max(res, moves[i])
-        res = max(res, nums[-1] - avg + moves[-1])
         
-        return int(res)
+        return ans
