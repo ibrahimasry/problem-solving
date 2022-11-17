65cf9d9aac601d59b555258dbc37bbcb921d4c0f
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-11-17 03:37:53 +0200

    Time: 306 ms (33.86%), Space: 13.9 MB (98.11%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,26 +1,17 @@
 class Solution:
     def racecar(self, target: int) -> int:
         
-        seen = set()
-        seen.add((0,1))
-        queue = deque()
-        queue.append((0,1))
+        dp = [sys.maxsize] * (target + 1)
         
-        steps = 0
-        while queue:
-            for _ in range(len(queue)):
-                pos, speed = queue.popleft()
-                newPos = pos + speed
-                if newPos == target:
-                    return steps + 1
-                newSpeed = 2 * speed
-                
-                if abs(newSpeed) < 2 * target and abs(newPos) < 2 * target and (newPos, newSpeed) not in seen:
-                    queue.append((newPos, newSpeed))
-                    seen.add((newPos, newSpeed))
-                newSpeed = -1 if speed > 0 else 1
-                if (pos, newSpeed) not in seen:
-                    queue.append((pos, newSpeed))
-                    seen.add((pos, newSpeed))
-            steps += 1
         
+        for t in range(target+1):
+            k = t.bit_length()
+            if t == 2 ** k - 1 :
+                dp[t] = k
+                continue
+            dp[t] = min(dp[t] , k + 1 + dp[2**k -1 - t])
+            
+            for j in range(k-1):
+                distance = t - 2 ** (k-1) + 2**j
+                dp[t] = min(dp[t] , k + 1 + j  + dp[distance])
+        return dp[target]
