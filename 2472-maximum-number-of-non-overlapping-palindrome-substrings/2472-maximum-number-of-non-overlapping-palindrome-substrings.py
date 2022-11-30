e0ea019678010b4fbcb47dfa819cf9bac68ac3ba
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-11-30 06:33:12 +0200

    Time: 51 ms (95.07%), Space: 13.9 MB (95.07%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        
+        n = len(s)
+        start = 0
+        ans = 0
+        
+        for c in range(2 * n -1):
+            left = c // 2
+            right = left + c % 2
+            while start <= left and right < n and s[left] == s[right]:
+                if right - left + 1 >= k :
+                    ans +=1
+                    start = right + 1
+                    break
+                left -= 1
+                right += 1
+        return ans
+        
