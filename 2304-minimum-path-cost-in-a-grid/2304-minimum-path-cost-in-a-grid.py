a7f8c53f952e6f899b57eedffd97cd48711fc3a3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-02 07:47:12 +0200

    Time: 2562 ms (87.47%), Space: 19.6 MB (93.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
@@ -1,18 +1,18 @@
 class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
+        m = len(grid[0])
+        n = len(grid)
         
-        @lru_cache(None)
-        def dfs (r, c):
-            if r == len(grid) - 1:
-                return grid[r][c]
-            minn = sys.maxsize
-            for j in range(len(grid[0])):
-                minn = min(minn, dfs(r+1, j) + moveCost[grid[r][c]][j] + grid[r][c])
-            return minn
-
-        minn = sys.maxsize
-        for i in range(len(grid[0])):
-            minn = min(dfs(0, i), minn)
+        dp = [0] * m
+        
+        for i in range(m):
+            dp[i] = grid[0][i]
             
-            
-        return minn
+        for i in range(1, n):
+            curr = [sys.maxsize] * m
+            for j in range(m):
+                for k in range(m):
+                    curr[j] = min(curr[j] , dp[k] + moveCost[grid[i-1][k]][j] + grid[i][j])
+            dp = curr
+        return min(dp)
+        
