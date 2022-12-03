5eeaaf3d517076bc7b6f57c7915e92e3e5a5442b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-03 09:15:49 +0200

    Time: 257 ms (68.18%), Space: 14.8 MB (81.82%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        
+        diff = count = 0
+        n = len(num)
+        for i, c in enumerate(num):
+            if i  < n // 2:
+                if c == "?":
+                    count += 1
+                else :
+                    diff += int(c)
+            else :
+                if c == "?":
+                    count -= 1
+                else :
+                    diff -= int(c)
+        if count > 0 and diff > 0:
+            return True
+        if count < 0 and diff < 0:
+            return True
+        return (abs(count)/2) * 9 != abs(diff)
