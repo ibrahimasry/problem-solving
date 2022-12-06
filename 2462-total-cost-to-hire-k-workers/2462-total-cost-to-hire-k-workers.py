a7285b4ad908ef976f5dd74cbadc9faccb5c927d
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-06 12:19:10 +0200

    Time: 2793 ms (23.07%), Space: 32.6 MB (13.53%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -10,18 +10,18 @@ class Solution:
         for i in range(max(candidates, n-candidates), n):
                 heapq.heappush(pq, (costs[i], i,1))
         ans  = 0
-        maxLeft = candidates 
-        minRight = n - candidates - 1
+        maxLeft = candidates - 1
+        minRight = n - candidates
         while k :
             cost, index, i = heapq.heappop(pq)
             ans += cost
             k -= 1
-            if maxLeft > minRight:
+            if( maxLeft + 1 >= minRight) :
                 continue
             if i == 1:
-                heapq.heappush(pq, (costs[minRight], minRight,1))
+                heapq.heappush(pq, (costs[minRight-1], minRight - 1,1))
                 minRight -= 1
             else :
-                heapq.heappush(pq, (costs[maxLeft], maxLeft, -1))
+                heapq.heappush(pq, (costs[maxLeft+1], maxLeft+1, -1))
                 maxLeft += 1
         return ans
