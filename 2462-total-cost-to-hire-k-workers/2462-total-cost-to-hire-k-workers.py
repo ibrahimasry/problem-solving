d8c70d3e5c38ee652d4cf5ff2a5cb16ba5a9cb84
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-06 12:20:12 +0200

    Time: 2174 ms (52.08%), Space: 32.6 MB (13.53%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -5,23 +5,23 @@ class Solution:
         n = len(costs)
         pq = []
         for i in range(min(candidates, n)):
-                heapq.heappush(pq, (costs[i],i, -1))
+                heapq.heappush(pq, (costs[i], -1))
                 seen.add(i)
         for i in range(max(candidates, n-candidates), n):
-                heapq.heappush(pq, (costs[i], i,1))
+                heapq.heappush(pq, (costs[i], 1))
         ans  = 0
         maxLeft = candidates - 1
         minRight = n - candidates
         while k :
-            cost, index, i = heapq.heappop(pq)
+            cost,  i = heapq.heappop(pq)
             ans += cost
             k -= 1
             if( maxLeft + 1 >= minRight) :
                 continue
             if i == 1:
-                heapq.heappush(pq, (costs[minRight-1], minRight - 1,1))
+                heapq.heappush(pq, (costs[minRight-1],1))
                 minRight -= 1
             else :
-                heapq.heappush(pq, (costs[maxLeft+1], maxLeft+1, -1))
+                heapq.heappush(pq, (costs[maxLeft+1], -1))
                 maxLeft += 1
         return ans
