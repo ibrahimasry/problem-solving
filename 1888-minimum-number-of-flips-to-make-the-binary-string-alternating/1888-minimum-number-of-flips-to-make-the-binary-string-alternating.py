56c9271113a25fd5f609775736eff62b94a904fe
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-08 20:47:59 +0200

    Time: 5448 ms (5.10%), Space: 21.9 MB (5.12%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -2,12 +2,12 @@ class Solution:
     def minFlips(self, s: str) -> int:
         s = s + s
         s = [int(c) for c in s]
-        def good(s):
+        def good(flip):
             
             prefix = [0]
             res = len(s)
             for i, c in enumerate(s):
-                if i % 2 == c:
+                if flip == c:
                     prefix.append(prefix[-1])
                 else :
                     prefix.append(prefix[-1] + 1)
@@ -15,7 +15,6 @@ class Solution:
                 if i >= (len(s) // 2):
                             
                     res = min(res, prefix[-1] - prefix[-1-(len(s)//2)])
-            
+                flip ^= 1
             return res
-        r = [1-c for c in s]
-        return min(good(s), good(r))
+        return min(good(0), good(1))
