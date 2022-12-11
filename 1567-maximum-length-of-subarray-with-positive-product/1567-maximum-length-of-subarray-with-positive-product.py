d08cbfb0c5c0d58f81dfd934128ac96e131f16f3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-12 01:40:36 +0200

    Time: 8589 ms (5.04%), Space: 28.2 MB (59.37%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -4,23 +4,24 @@ class Solution:
         res = 0
         
         n = len(nums)
-        curr = 1
+        left = 1
+        right = 1
         start = 0
+        end = n -1
         for i in range(n):
-            num = nums[i]
-            curr *= num
-            if curr > 0:
+            left *= nums[i]
+            right *= nums[~i]
+            
+            j = n - 1 - i
+            if left > 0:
                 res = max(i - start + 1, res)
-            if curr == 0:
-                curr = 1
+            if right > 0:
+                res = max(end - j + 1, res)
+
+            if left == 0:
+                left = 1
                 start = i + 1
-        start = n-1
-        curr = 1
-        for i in range(n-1, -1, -1):
-            curr *= nums[i]
-            if curr > 0:
-                res = max(start - i + 1, res)
-            if curr == 0:
-                start = i -1
-                curr = 1
+            if right == 0:
+                end = j - 1
+                right = 1
         return res
