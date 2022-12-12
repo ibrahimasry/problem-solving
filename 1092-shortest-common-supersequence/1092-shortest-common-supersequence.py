5f3b60c38ef80db64774ca59d402f32733408bf9
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-12 23:28:28 +0200

    Time: 667 ms (75.26%), Space: 29.3 MB (36.86%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -3,11 +3,7 @@ class Solution:
         n = len(str1)
         m = len(str2)
     
-        if n > m:
-            return self.shortestCommonSupersequence(str2, str1)
         dp = [[""] * (m +1)for _ in range(n+1)]
-        
-        
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if str1[i-1] == str2[j-1]:
