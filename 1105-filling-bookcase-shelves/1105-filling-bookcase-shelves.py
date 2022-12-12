11a5c040601a2cbc940a3899a8197a6bf0b0dcb3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-12 21:52:11 +0200

    Time: 55 ms (90.29%), Space: 14.2 MB (72.76%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,15 +1,17 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        @lru_cache(None)
-        def dp(i, w, h):
-            if i == n :
-                return  h
-            
-            
-            res =  h +  dp(i+1, shelfWidth - books[i][0] , books[i][1])
-            
-            if w - books[i][0] >= 0 :
-                res = min(res, dp(i+1, w-books[i][0] , max(books[i][1], h)))
-            return res
         n = len(books)
-        return dp(0, 0, 0)
+        dp = [sys.maxsize] * (n+1)
+        dp[0] = 0
+        
+        for i in range(1, n+1):
+            currWidth = shelfWidth
+            currHeight = 0
+            for j in range(i,0,-1):
+                w, h = books[j-1]
+                if currWidth - w < 0:
+                    break
+                currWidth  -= w
+                currHeight = max(currHeight, h)
+                dp[i] = min(dp[i], dp[j-1] + currHeight)
+        return dp[-1]
