6bb24b60eafd430c1ddfe5136ac3bd727726f525
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-12 22:09:47 +0200

    Time: 649 ms (86.55%), Space: 14 MB (71.49%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -3,17 +3,14 @@ class Solution:
         
         
         n = len(arr)
-        dp = [0] * n
+        dp = [0] * (n+1)
         
         
-        for i in range(n):
+        for i in range(1, n + 1):
             curr = -sys.maxsize
             for j in range(k):
-                if i - j < 0 :
-                    continue
-                prev = arr[i-j] if i - j >= 0 else 0
-                curr = max(curr, prev)
-                prevArr = dp[i-j-1] if i-j-1 >= 0 else 0
-                dp[i] = max(curr * (j+1) + prevArr, dp[i])
+                if i - j - 1 >= 0 :
+                    curr = max(curr, arr[i-j-1])
+                    dp[i] = max(curr * (j+1) +  dp[i-j-1] , dp[i])
                 
         return dp[-1]
