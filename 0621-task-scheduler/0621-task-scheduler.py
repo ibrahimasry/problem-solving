c0cc9a111ca0f026794fb90b6fa14ec0e646a9a1
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-13 17:02:31 +0200

    Time: 1101 ms (51.62%), Space: 14.5 MB (42.49%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -5,12 +5,12 @@ class Solution:
         freq = Counter(tasks)
         
         maxKey,maxKeyValue = max(freq.items(),key=lambda x:x[1])
-        total = sum(freq.values())
-        
         maxKeyCount = sum([1 for k, v in freq.items() if v == maxKeyValue])
+
+        total = len(tasks)
+        
         gap = (n - (maxKeyCount-1)) * (maxKeyValue -1)
         gap -= (total- maxKeyCount * maxKeyValue)
         gap = 0 if gap < 0 else gap
         return total + gap
         
-        return res
