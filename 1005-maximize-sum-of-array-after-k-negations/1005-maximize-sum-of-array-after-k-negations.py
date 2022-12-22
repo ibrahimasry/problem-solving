d803f93e76dc3ba5a43b929158724c48ca8103b6
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-22 08:12:39 +0200

    Time: 51 ms (93.06%), Space: 13.9 MB (60.49%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -1,30 +1,12 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        pos = [n for n in nums if n > 0]
-        neg = sorted([abs(n) for n in nums if n < 0])
-        zeros = sum([1 for n in nums if n == 0])
-        posTotal = sum(pos)
-        negTotal = sum(neg)
-        
-        
-        if len(neg) == 0 and zeros:
-            return posTotal
-        if len(neg) == 0 :
-            if k%2:
-                return posTotal - 2 * min(nums)
-            else:
-                return posTotal
-        if len(neg) == k:
-            return posTotal + negTotal
-        if len(neg) < k and zeros:
-            return negTotal + posTotal
-        if len(neg) < k :
-            if (len(neg)  - k) % 2 == 0:
-                return sum(pos) + sum(neg) 
-            return sum(pos) + sum(neg) - 2 * min(min(pos + [1001]) , min(neg + [1001]))
-        if len(neg) > k:
-            return sum(pos) + sum(neg) - 2 * sum(neg[:-k])
-        
+        nums.sort()
+        i = 0
+        while k and i < len(nums) and nums[i] < 0:
+            nums[i] = -nums[i]
+            i += 1
+            k -= 1
+        return sum(nums) - (k % 2) * 2 * min(nums)
         
         
         
