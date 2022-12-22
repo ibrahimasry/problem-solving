35b6dad85da6fcd93d2d2fa334fb15db1996acae
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-22 21:37:50 +0200

    Time: 114 ms (68.52%), Space: 14.6 MB (57.41%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
@@ -2,7 +2,7 @@ class Solution:
     def minFlips(self, target: str) -> int:
         count = 0
         
-        for i, c in enumerate(target):
+        for c in target:
             if int(c) != count % 2:
                 count += 1
         return count
