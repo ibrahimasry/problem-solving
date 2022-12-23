776db0e2c22134105852f6a11e1b99f0ebf8b451
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-23 21:29:21 +0200

    Time: 80 ms (75.37%), Space: 14.7 MB (39.60%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -2,18 +2,13 @@ class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
         @cache
         def dp(s1,s2):
-            if not s1:
-                return True
-            if len(s1) == 1:
-                return s1 == s2
             if s1 == s2:
                 return True
-            if Counter(s1) != Counter(s2):
-                return False
-            for i in range(1,len(s1)):
-                if dp(s1[:i],s2[:i]) and dp(s1[i:],s2[i:]):
-                    return True
-                if dp(s1[:i],s2[-i:]) and dp(s1[i:],s2[:-i]):
-                    return True
+            if Counter(s1) == Counter(s2):
+                for i in range(1,len(s1)):
+                    if dp(s1[:i],s2[:i]) and dp(s1[i:],s2[i:]):
+                        return True
+                    if dp(s1[:i],s2[-i:]) and dp(s1[i:],s2[:-i]):
+                        return True
             return False
         return dp(s1,s2)
