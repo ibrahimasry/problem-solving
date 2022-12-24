4ea1af1318997f6e92244b48287f80658117040e
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-24 07:10:13 +0200

    Time: 347 ms (82.07%), Space: 15.2 MB (14.52%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -6,21 +6,11 @@ class MyCalendar:
 
     def book(self, start: int, end: int) -> bool:
         books = self.books
-        idx = bisect.bisect_left(books,start+1 , key=lambda x:x[1])
+        idx = bisect.bisect_left(books , start+1 , key=lambda x:x[1])
         if idx == len(self.books) or books[idx][0] >= end :
             books.add((start,end))
             return True
         return False
-    def bst(self ,books, start):
-        l = 0
-        r = len(books)
-        while l < r:
-            m = l + (r-l)//2
-            if books[m][1] > start:
-                r = m 
-            else :
-                l = m + 1
-        return l
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
