7cd30567aebe3175790e3715f118a07d6a34e555
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-25 07:09:04 +0200

    Time: 60 ms (62.50%), Space: 14 MB (75.00%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -4,13 +4,11 @@ class Solution:
         if target not in set(words):
             return -1
         res = n
-        last = inf
         s1 =  s + n 
 
         for i in range(n):
             if words[i] == target:
                 last = i
-            if last != inf:
                 res = min(res, min(last + n - s, min(abs(last - s), abs(last-s1))))
 
         return res
