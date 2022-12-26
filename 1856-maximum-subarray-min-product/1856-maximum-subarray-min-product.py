c3e3f906267ccb64ef9311533921cf553b1e4a96
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-26 06:45:25 +0200

    Time: 1350 ms (79.55%), Space: 26.2 MB (85.62%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
@@ -3,16 +3,15 @@ class Solution:
         prefix = [0]
         for n in nums:
             prefix.append(prefix[-1] + n)
-        s = []
+        s = [-1]
         
         res = 0
         
         for i in range(0, len(nums) + 1):
-            while s and (i == len(nums) or  nums[s[-1]] >= nums[i] ):
+            while len(s) > 1 and (i == len(nums) or  nums[s[-1]] >= nums[i] ):
 
                 curr = nums[s.pop()]
-                left = (0 if len(s) == 0 else s[-1]+1)
-                total = prefix[i] - prefix[left]
+                total = prefix[i] - prefix[s[-1] + 1]
                 res = max(res, total * curr)
             s.append(i)
         return res % (10 ** 9 + 7)
