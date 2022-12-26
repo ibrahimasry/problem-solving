1ab3fb1388b6d87a12c779b0a0824a05d92f6105
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-26 07:45:40 +0200

    Time: 31 ms (98.61%), Space: 13.9 MB (27.52%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.py
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.py
@@ -4,8 +4,8 @@ class Solution:
         start = 0
         res = []
         for i , (c1, c2) in enumerate(zip(s,s[1:])):
-            if c1 != c2:
-                if (i - start) + 1 >= 3:
-                    res.append([start, i])
-                start = i + 1
+            if c1 == c2: continue
+            if (i - start) + 1 >= 3:
+                res.append([start, i])
+            start = i + 1
         return res
