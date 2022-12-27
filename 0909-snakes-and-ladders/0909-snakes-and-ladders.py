fd8e0b703785b695254a358a949f266964c80211
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-27 03:48:17 +0200

    Time: 127 ms (86.85%), Space: 13.8 MB (95.57%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -9,24 +9,18 @@ class Solution:
         while q:
             for _ in range(len(q)):
                 curr = q.popleft()
-                if curr in seen == True:
-                    continue
-                seen.add(curr)
                 if curr == (n * n) - 1:
                     return steps
 
-                for pos in range(curr + 1, curr + 7):
-                    if pos >= n * n: 
-                        continue
+                for pos in range(curr + 1, min(curr + 7, n*n)):
                     r , c = pos // n , pos % n
                     c = (c if r % 2 == 0 else ((n - 1) - c))
                     rVal = (m - r) - 1
                     cVal = c
                     nex = board[rVal][cVal] -1
-
                     i = pos if nex < 0 else nex
-
                     if (i in seen) == False:
                         q.append(i)
+                        seen.add(i)
             steps += 1                
         return -1
