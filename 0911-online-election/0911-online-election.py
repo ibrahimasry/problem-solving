0b3fe5b2b76ee9b85d16a3ce76a1f9bd5b27f63d
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-28 01:48:51 +0200

    Time: 643 ms (88.79%), Space: 19.7 MB (88.18%) - LeetHub

---
+++ 0911-online-election/0911-online-election.py
@@ -0,0 +1,27 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.leaders = [0] * len(persons)
+        self.count = defaultdict(int)
+        leader= 0
+        maxTotal = 0
+        leaders = self.leaders
+        count = self.count
+        for p, t in zip(persons, times):
+            count[p] += 1
+            if count[p] >= maxTotal:
+                leader = p
+                maxTotal = count[p]
+            leaders.append((t,leader))
+
+
+    def q(self, t: int) -> int:
+        
+        idx = bisect.bisect(self.leaders, (t,inf)) - 1
+        return self.leaders[idx][1]
+        
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
