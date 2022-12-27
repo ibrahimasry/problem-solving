961b09818fa2858ec4c7e33662259a2ff90392a3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-27 04:14:17 +0200

    Time: 41 ms (94.94%), Space: 16.3 MB (53.09%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -1,14 +1,3 @@
 class Solution:
     def arrangeWords(self, text: str) -> str:
-        hash = defaultdict(list)
-        res = ''
-        for word in text.split():
-            hash[len(word)].append(word)
-        for l in sorted(hash):
-            sent = ' '.join(hash[l])
-            if len(res):
-                res += " " +  sent
-            else :
-                res += sent
-        res = res[0].upper() + res[1:].lower()
-        return res
+        return " ".join(sorted(text.split(), key=len)).capitalize()
