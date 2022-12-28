00c0b670af22c296485b594b002f2929c6c75acc
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-28 06:30:03 +0200

    Time: 29 ms (93.41%), Space: 14 MB (35.33%) - LeetHub

--- 0640-solve-the-equation/0640-solve-the-equation.py
+++ 0640-solve-the-equation/0640-solve-the-equation.py
@@ -13,28 +13,31 @@ class Solution:
         while i < k:
             curr = equation[i]
             if curr in ["-", "+"]:
-                n = 1
                 if curr == "-":
                     prev = -1
                 else:
                     prev = 1 
             elif curr == "=":
-                n = 1
                 prev = 1
                 flag   = 1
                 flagx  = -1
             
             elif curr == "x":
-                x.append(prev * n * flagx)
-                n = 1
+                x.append(prev * flagx)
             else :
                 n = 0
                 while i < k and equation[i].isdigit():
                     n = n * 10 + int(equation[i])
                     i += 1
                 i -= 1
-                if i < k -1 and equation[i+1] != "x":
+                if (i+1 < k  and equation[i+1] != "x" ) or i+1 == k:
                     nums.append(prev * n * flag)
+                elif i + 1 < k and equation[i+1] == "x" :
+                    x.append(prev * n * flagx)
+                    i += 1
+                n = 1
+
+                    
 
             i += 1
         if sum(x) == 0 and sum(nums) != 0:
