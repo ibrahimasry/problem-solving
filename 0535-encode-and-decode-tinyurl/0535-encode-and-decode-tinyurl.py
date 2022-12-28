681302d6517b7e3c129b3443552fb55c31fe340b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-28 04:37:40 +0200

    Time: 35 ms (87.90%), Space: 13.9 MB (67.90%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,7 +1,6 @@
 class Codec:
     alpha = string.ascii_letters + "123456790"
     def __init__(self):
-        self.longToCode  = {}
         self.codeToLong = {}
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
@@ -9,7 +8,6 @@ class Codec:
         res = "http://tinyurl.com/"
         path = longUrl.split('/')[1:]
         code = str([random.choice(self.alpha) for i in range(6)])
-        self.longToCode[longUrl] = code
         self.codeToLong[code] = longUrl
         return res + code
 
