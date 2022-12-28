23b026e98e61406592981438619a1c2458661235
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-28 04:50:54 +0200

    Time: 226 ms (76.68%), Space: 15.4 MB (74.23%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        xor , xor1 , xor2=[0] * 3
+        
+        for n in nums:
+            xor ^= n
+        for i in range(1,len(nums)+1):
+            xor ^= i
+        right = (xor & ~(xor-1))
+        
+        for i in nums:
+            if right & i != 0:
+                xor2 ^= i
+            else:
+                xor1 ^= i
+        for i in range(1,len(nums)+1):
+            if right & i != 0:
+                xor2 ^= i
+            else:
+                xor1 ^= i
+        for n in nums:
+            if n == xor2:
+                return [xor2,xor1]
+        return [xor1,xor2]
