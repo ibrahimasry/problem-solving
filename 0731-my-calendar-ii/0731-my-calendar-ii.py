7f462b013523505255212f8aef7ecba9597c05b9
ibrahimasry <ibrahim.elmansoury@gmail.com>
2022-12-31 08:33:47 +0200

    Time: 2268 ms (34.66%), Space: 15.2 MB (22.26%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -2,19 +2,13 @@ from sortedcontainers import SortedDict
 class MyCalendarTwo:
 
     def __init__(self):
-        self.mapped = SortedDict()
+        self.mapped = SortedDict(defaultdict(lambda:0))
 
     def book(self, start: int, end: int) -> bool:
         
         mapped = self.mapped
-        if start in mapped:
-            mapped[start] += 1
-        else :
-            mapped[start] = 1
-        if end in mapped:
-            mapped[end] -= 1
-        else :
-            mapped[end] = -1
+        mapped[start] =  mapped[start] + 1 if start in mapped else 1
+        mapped[end]   =   mapped[end] - 1 if end in mapped else -1
         overlapped = 0
         
         for  val in mapped.values():
