fa006532dc2b5e208d00f63d7d47aec90aa1cfe0
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-01 08:22:48 +0200

    Time: 3805 ms (62.50%), Space: 28.9 MB (87.50%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -1,20 +1,18 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
         primes = [True] * (right + 1)
-        primes[0] = primes[1] = False
         n1 = 1
         n2 = prev = -1
         
         for i in range(2, right + 1):
             if primes[i]:
                 if i >= left:
-                    if prev != -1:
-                        if n2 - n1 > i - prev or (n2 * n1 < 0):
-                            n1 , n2 = prev , i
+                    if n2 - n1 > i - prev or (n2 * n1 < 0):
+                        n1 , n2 = prev , i
                     prev = i
                 for j in range(i*i, right+1 , i):
                     primes[j] = False
         if n2 * n1 < 0:
             return [-1,-1]
-        return [n1,n2]
+        return [n1 , n2]
         
