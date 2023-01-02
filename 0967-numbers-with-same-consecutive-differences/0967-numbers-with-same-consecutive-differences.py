2d3509953d8393426ba77fb49ae7679c6e4b4003
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-02 06:31:33 +0200

    Time: 15 ms (100.00%), Space: 13.6 MB (84.85%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        q = deque(list(range(1,10)))
+        q = deque((range(1,10)))
         while n > 1:
             for _ in range(len(q)):
                 curr = q.popleft()
                 r = curr % 10
                 if r + k < 10:
                     q.append(curr * 10 + (r + k))
-                if r - k >= 0:
+                if r - k >= 0 and k > 0:
                     q.append(curr * 10 + (r - k))
             n -= 1
-        return set(q)
+        return q
