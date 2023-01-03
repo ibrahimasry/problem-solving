c180f25c129ea1adfc76501109c4f8aa9ca835d3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-03 04:26:54 +0200

    Time: 438 ms (35.79%), Space: 14.6 MB (61.89%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -3,9 +3,9 @@ class Solution:
         m = len(s)
         n = len(s[0])
         res = 0
-        seen = set()
-        for i in range(1,m):
-            for j in range(n):
+        for j in range(n):
+            for i in range(1,m):
                 if s[i][j] < s[i-1][j]:
-                    seen.add(j)
-        return len(seen)
+                    res += 1
+                    break
+        return res
