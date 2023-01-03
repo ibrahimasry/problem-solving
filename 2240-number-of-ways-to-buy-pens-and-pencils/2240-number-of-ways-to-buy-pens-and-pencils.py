708074949a46e1e4ec57190424e4e2a68e011a20
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-03 09:36:44 +0200

    Time: 858 ms (67.46%), Space: 13.9 MB (68.25%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.py
@@ -0,0 +1,7 @@
+class Solution:
+    def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
+        res = 0
+        
+        for c in range(total//cost1 + 1):
+            res += (((total-c * cost1)//cost2) + 1)
+        return res
