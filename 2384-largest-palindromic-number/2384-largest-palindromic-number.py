5d9f9a792744fba2cc870f9cc13bcd04edb95db3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-03 06:35:10 +0200

    Time: 805 ms (11.81%), Space: 15.4 MB (32.84%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -7,11 +7,11 @@ class Solution:
         for i in range(9,-1,-1):
             if count[str(i)] > 1:
                 if i == 0 and not pal:
-                    continue
+                    break
                 pal += str(i) *  int(count[str(i)] //2)
         for i in range(9,-1,-1):
             if count[str(i)] % 2:
                 return str(int(pal + str(i) + pal[::-1]))
         if not pal:
             return '0'
-        return str(int(str(pal + pal[::-1])))
+        return str(int(pal + pal[::-1]))
