66a44a73bc5af5b68c8fdaf4c2a22214c168e9a2
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-03 06:32:07 +0200

    Time: 728 ms (13.65%), Space: 15.3 MB (32.84%) - LeetHub

---
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestPalindromic(self, num: str) -> str:
+        count = Counter(num)
+        
+        pal = ""
+        
+        for i in range(9,-1,-1):
+            if count[str(i)] > 1:
+                if i == 0 and not pal:
+                    continue
+                pal += str(i) *  int(count[str(i)] //2)
+        for i in range(9,-1,-1):
+            if count[str(i)] % 2:
+                return str(int(pal + str(i) + pal[::-1]))
+        if not pal:
+            return '0'
+        return str(int(str(pal + pal[::-1])))
