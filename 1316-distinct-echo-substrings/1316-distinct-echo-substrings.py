855580d3099eedafc5f8fd0169ce2d265a70a87b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-04 04:12:15 +0200

    Time: 4328 ms (42.42%), Space: 14.2 MB (90.91%) - LeetHub

--- 1316-distinct-echo-substrings/1316-distinct-echo-substrings.py
+++ 1316-distinct-echo-substrings/1316-distinct-echo-substrings.py
@@ -1,24 +1,29 @@
 class Solution:
     def distinctEchoSubstrings(self, text: str) -> int:
+        
+        
+        def getHash(start, end):
+            if start == 0:
+                return hash[end]
+            return hash[end] - hash[start-1] * powers[end-start + 1]
         mod = (10**9) + 7
         n = len(text)
-        hash = [0] * (n+1)
-        powers = [0] * (n+1)
-        
-        powers[0] = 1
-        
+        hash = [0] * (n)
+        powers = [0] * (n)
+        prevHash = 0
+        prevPower = 1
         for i, c in enumerate(text):
             code = ord(c) 
-            hash[i+1] = (hash[i] * 256 + code) % mod
-            powers[i+1] = (powers[i] * 256) % mod
+            hash[i] = prevHash = (prevHash * 256 + code) % mod
+            powers[i] = prevPower = 1 if i == 0 else  prevPower * 256 % mod
         seen = set()
         for i , c in enumerate(text):
             for l in range(2, n+1, 2):
                 if i + l > n:
                     break
-                f = i + l // 2
-                s1 = ((hash[f] - hash[i] * powers[(f-i)]) + mod) % mod
-                s2 = ((hash[i+l] - hash[f] * powers[((i+l)-f)]) + mod) % mod
+                mid = i + l//2 -1
+                s1= getHash(i, mid) % mod
+                s2 = getHash(mid+1, i+l-1) % mod
                 if s1 == s2:
                     seen.add(s1)
         return len(seen)
