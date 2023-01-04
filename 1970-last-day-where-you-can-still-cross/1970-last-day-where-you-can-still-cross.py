9b82ac354713e817a098d5d5a51ac215c9d715ec
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-04 05:40:26 +0200

    Time: 3561 ms (57.45%), Space: 22.7 MB (94.68%) - LeetHub

---
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -0,0 +1,35 @@
+class Solution:
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        
+        def good(days):
+            mat = [[0] * col for _ in range(row)]
+            for i in range(days):
+                mat[cells[i][0]-1][cells[i][1]-1] = 1
+            q = deque()
+            for j in range(col):
+                if mat[0][j] == 0:
+                    q.append((0,j))
+                    mat[0][j] = 1
+            curr = 0
+            while q:
+                i,j = q.popleft()
+                if i == row-1:
+                    return True
+                for x,y in [[1,0],[-1,0],[0,-1],[0,1]]:
+                    r = i + x
+                    c = j + y
+                    if 0 <= r < row and 0 <= c < col and mat[r][c] == 0:
+                        q.append((r,c))
+                        mat[r][c] = 1
+            return False
+        
+        
+        l = 1
+        r = len(cells)
+        while l < r:
+            m = (l+r+1)>>1
+            if good(m):
+                l = m
+            else:
+                r = m - 1
+        return l
