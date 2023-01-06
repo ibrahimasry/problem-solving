6e9435501fb70d2103b5d1191248578126bc2d51
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-06 16:12:17 +0200

    Time: 5499 ms (42.00%), Space: 14.4 MB (80.00%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.py
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.py
@@ -1,6 +1,6 @@
 class Solution:
     def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
-        mapped = defaultdict(set)
+        mapped = {c:set() for c in string.ascii_letters + "0123456789"} 
         for k , v in mappings:
             mapped[k].add(v)
         
@@ -8,13 +8,10 @@ class Solution:
             for k in range(len(sub)):
                 c = s[i+k]
                 c1 = sub[k] 
-                
-                if c1 != c and not (c1 in mapped and c in mapped[c1]):
+                if c1 != c and  c not in mapped[c1]:
                     break
-                
                 if k == len(sub)-1:
                     return True
-
         return False
             
                 
