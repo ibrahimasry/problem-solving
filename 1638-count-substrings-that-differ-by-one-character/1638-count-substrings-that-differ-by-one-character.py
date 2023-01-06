c359d964113defce20790bb7cd744eba7d702382
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-06 06:39:15 +0200

    Time: 1765 ms (22.16%), Space: 13.8 MB (98.80%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
@@ -6,16 +6,12 @@ class Solution:
         
         for i in range(len(s)):
             for j in range(i,len(s)):
-                curr = s[i:j+1]
                 m = 0
-                l = len(curr)
-                q = deque("-"+t[:len(curr)-1])
+                l = j - i + 1
                 while m < (len(t)-l) + 1:
                     found = 0
-                    q.popleft()
-                    q.append(t[(m+l) - 1])
                     for k in range(l):
-                        if q[k] != curr[k] :
+                        if t[m+k] != s[i + k] :
                             found += 1
                             if found > 1:
                                 break
