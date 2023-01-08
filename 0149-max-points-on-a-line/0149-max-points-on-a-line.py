22d01fcc784ad2ad61bb5444a23be6aca8230eb3
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-08 08:43:23 +0200

    Time: 98 ms (85.12%), Space: 14 MB (75.79%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -5,12 +5,12 @@ class Solution:
         res = 1
         points.sort()
         for i in range(n):
-            p1 ,p2= points[i]
-            count = defaultdict(lambda :1)
+            p1 ,p2 = points[i]
+            count  = defaultdict(lambda :1)
             for j in range(i+1,n):
                 p3, p4 = points[j]
                 x,y = p1-p3 , p2-p4
                 z = gcd(abs(x),abs(y))
                 count[(x // z,y // z)] += 1
-                res = max(res, max(list(count.values()) + [0]))
+                res = max(res, count[(x//z,y//z)])
         return res
