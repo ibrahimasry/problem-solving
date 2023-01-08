b9749f16312d874c594b4213bdb10ae45ab7259b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-08 08:29:54 +0200

    Time: 221 ms (46.80%), Space: 13.9 MB (94.26%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -13,17 +13,11 @@ class Solution:
                 p3,p4 = points[j]
                 if p1 == p3 and p4 == p2:
                     overlap += 1
-                elif p2 == 0 and p4 == 0:
-                    count[(1,0)] += 1
-                elif p1 == 0 and p3 == 0:
-                    count[(0,1)] += 1
                 else:
                     x,y = p1-p3,p2-p4
                     z = gcd(abs(x),abs(y))
                     count[(x//z,y//z)] += 1
-                values = list(count.values())
-                values.append(0)
-                maxVal = max(values)
+                values = [0] + list(count.values())
             
-                res = max(res,maxVal + 1 + overlap)
+                res = max(res, max(values) + 1 + overlap)
         return res
