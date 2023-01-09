1f0eb4719a2f686da999acd939786cd48103e674
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-09 04:18:12 +0200

    Time: 40 ms (64.52%), Space: 13.9 MB (25.40%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
@@ -1,19 +1,17 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
         num = str(num)
+        count = Counter(num)
+        res = ""
+
         if num[0].isdigit():
-            nums = [1,2,3,4,5,6,7,8,9]
-            count = Counter(num)
-            res = ""
-            for i in nums:
+            for i in range(1,10):
                 if count[str(i)] > 0:
                     res += str(i) * count[str(i)]
             if count['0'] > 0:
                 return int(res[:1] + '0' * count[str(0)] + res[1:])
             return int(res)
         else :
-            res = ""
-            count = Counter(num)
             for i in range(9,-1,-1):
                 if count[str(i)] > 0:
                     res += str(i) * count[str(i)]
