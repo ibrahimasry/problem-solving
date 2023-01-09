e61d33c33a10de00644a8104e456cc277e88f34d
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-09 04:38:14 +0200

    Time: 178 ms (93.49%), Space: 14.3 MB (98.60%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -0,0 +1,9 @@
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        res = []
+        for i in range(1,n+1):
+            for j in range(1,i):
+                if gcd(i,j) == 1:
+                    res.append(str(j) + "/" + str(i))
+                    
+        return res
