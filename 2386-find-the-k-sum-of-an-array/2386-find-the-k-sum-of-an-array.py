f6e6b830a7a1318d6c5f9ad180ba90dc00472759
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-09 06:12:24 +0200

    Time: 808 ms (82.62%), Space: 28.5 MB (73.76%) - LeetHub

--- 2386-find-the-k-sum-of-an-array/2386-find-the-k-sum-of-an-array.py
+++ 2386-find-the-k-sum-of-an-array/2386-find-the-k-sum-of-an-array.py
@@ -1,17 +1,15 @@
 class Solution:
     def kSum(self, nums: List[int], k: int) -> int:
         pos = sum([num for num in nums if num > 0])
-        if k == 1:
-            return pos
+        if k == 1: return pos
 
         nums = [abs(num) for num in nums]
         nums.sort()
         pq = [[nums[0],0]]
-        while k>2:
+        for _ in range(3,k+1):
             s , i = heapq.heappop(pq)
             if i < len(nums) -1:
                 heapq.heappush(pq, [s + nums[i+1],i+1 ])
                 heapq.heappush(pq, [(s - nums[i]) + nums[i+1], i+1 ])
-            k -= 1
         return pos - pq[0][0]
         
