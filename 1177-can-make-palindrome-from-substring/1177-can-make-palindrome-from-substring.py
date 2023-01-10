9d745315b8d2359df8508b2402a75d492d578b34
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-10 12:05:04 +0200

    Time: 1985 ms (84.05%), Space: 60.8 MB (85.28%) - LeetHub

---
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canMakePaliQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
+        prefix = [0]
+        ans = []
+        for c in s:
+            code = ord(c) - ord('a')
+            prefix.append(prefix[-1] ^ (1 << code))
+        for l,r,c in queries:
+            count = 0
+            curr = prefix[r+1] ^ prefix[l]
+            for i in range(32):
+                if (curr >> i) & 1:
+                    count += 1
+            if ((count - 1)/2 >  c) :
+                ans.append(False)
+            else:
+                ans.append(True)
+        return ans
