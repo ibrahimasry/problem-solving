a1345630d05fafc4e2266c043d6597d31c2c4090
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-11 14:34:55 +0200

    Time: 263 ms (92.21%), Space: 24.6 MB (98.36%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -1,49 +1,8 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        
-        def getMaxAll(arr):
-            run = res = minn = 0
-            
-            for n in arr:
-                if run <= 0 :
-                    run = minn = 0
-                run += n
-                res = max(run-minn,res)
-                minn = min(minn, n)
-            return res
-        def getMax(arr):
-            prefix = [0] + list(accumulate(arr))
-            currMax = -inf
-            running = -inf
-            currMin = inf
-            i = 0
-            for j, n in enumerate(arr):
-                if n < 0  and n <= currMin:
-                    if currMin == inf:
-                        currMin = n
-                        i = j
-                    else:
-                        if running == -inf:
-                            running = currMin
-                        else :
-                            if running + currMin < prefix[j] - prefix[i+1]:
-                                running = prefix[j] - prefix[i]
-                            else :
-                                running += currMin
-                        i = j
-
-                        currMin = n
-                elif running + n < n:
-                    running = n
-                    currMin = inf
-                     
-                else :
-                    running += n
-                currMax = max(running ,currMax)
-            return 0 if currMax == -inf else currMax
-        if min(arr) >= 0 or len(arr) == 1 : 
-            return sum(arr)
-        if not any([1 for n in arr if n > 0]):
-            return max(arr)
-            
-        return max(getMax(arr), getMaxAll(arr) , getMax(arr[::-1]))
+        res = zero = one = -inf
+        for x in arr:
+            one = max(zero, x + one)
+            zero = max(x, x + zero)
+            res = max(res, one, zero)
+        return res
