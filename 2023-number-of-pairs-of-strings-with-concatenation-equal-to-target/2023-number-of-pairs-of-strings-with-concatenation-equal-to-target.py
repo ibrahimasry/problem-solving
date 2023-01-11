a21fb14f517ebf9e26016c70bd4959f7d92b5cfd
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-11 09:12:12 +0200

    Time: 35 ms (99.00%), Space: 14 MB (30.94%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -1,26 +1,14 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
         
-        
-        hashl = defaultdict(int)
-        hashr = defaultdict(int)
-        for s in nums:
-            l = len(target)
-            if target.find(s) >= 0 :
-                l = target.index(s)
-            r = len(target)
-            if target.rfind(s)  >= 0:
-                r = target.rindex(s)
-            if l == 0:
-                hashl[s] += 1
-            if r == len(target) - len(s) :
-                hashr[s] += 1
+        freq = Counter(nums)
         res = 0
-        for key in hashl:
+        for key in freq:
+            if target[:len(key)] != key: continue
             l = key
             r = target[len(key):]
             if l == r:
-                res += hashl[key] * (hashl[key] - 1) 
+                res += freq[key] * (freq[key] - 1) 
             else :
-                res += hashl[key] * hashr[target[len(key):]]
+                res += freq[key] * freq[r]
         return res
