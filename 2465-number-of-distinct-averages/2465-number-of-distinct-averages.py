601c62c81b78194669bfef7aa932791f372c7067
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-12 09:53:51 +0200

    Time: 42 ms (73.19%), Space: 13.8 MB (97.49%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
@@ -0,0 +1,9 @@
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        seen = set()
+        total = sum(nums)
+        nums.sort()
+        n = len(nums)
+        for i in range((n//2)):
+            seen.add((nums[i] + nums[~i]) / 2.0)
+        return len(seen) 
