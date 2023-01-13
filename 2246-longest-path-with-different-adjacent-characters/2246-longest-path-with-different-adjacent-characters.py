1112e2b41a4c666403c6de8fd25ecf21f4593236
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-13 09:49:42 +0200

    Time: 1881 ms (80.77%), Space: 158.3 MB (26.92%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -10,20 +10,18 @@ class Solution:
         pairs = {v:c for v,c in enumerate(s)}
         ans=0
         
-        def dfs(node, parent):
-            
-            
+        def dfs(node):
             nonlocal ans
             l = 0
             r = 0
             for nei in graph[node]:
-                curr = dfs(nei,node)
+                curr = dfs(nei)
                 if pairs[nei] != pairs[node]:
                     if l  <= curr:
                         l , r = curr , l 
                     elif curr > r:
                         r = curr
-            ans = max(l+r+1, max(l+1,ans))
+            ans = max(l+r+1, ans)
             return l + 1
-        dfs(0,-1)
+        dfs(0)
         return ans
