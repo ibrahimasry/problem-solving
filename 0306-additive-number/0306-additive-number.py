2ae618cce28aec940b4dc1cff354b7a27bd6ca33
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-13 10:46:44 +0200

    Time: 41 ms (59.24%), Space: 14 MB (31.63%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -3,25 +3,20 @@ class Solution:
         
         if len(num) < 3:
             return False
-        def dfs(i , seq , ids):
-            if i + len(seq) >= n :
-                if ids == "f":
-                    return False
-                return True
+        def dfs(i , seq ):
             for j in range(i+1,n+1):
                 if num[i] == "0" and j != i+1:
                     return False
                 if num[j:].startswith(str(int(num[i:j]) + int(seq))):
                     if str(int(num[i:j]) + int(seq)) == num[j:]:
                         return True
-                    if dfs(j, num[i:j] , 's'):
+                    if dfs(j, num[i:j]):
                         return True
             return False
         n = len(num)
         for i in range(1, n):
             if num[0] == "0" and  i > 1:
                     return False
-
-            if  dfs(i, num[:i] , 'f'):
+            if  dfs(i, num[:i]):
                 return True
         return False
