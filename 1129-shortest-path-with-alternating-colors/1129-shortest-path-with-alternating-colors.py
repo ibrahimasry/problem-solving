a4daf4dff9703a8a2be39afd25ba887837681ca8
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-14 14:48:34 +0200

    Time: 118 ms (54.11%), Space: 14.3 MB (9.38%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,24 +1,19 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         
-        red = defaultdict(list)
-        blue = defaultdict(list)
+        graph = [defaultdict(list) for _ in range(2)]
         ans = [[inf] * n for _ in range(2)]
-        for u,v in redEdges:
-            red[u].append(v)
-        for u,v in blueEdges:
-            blue[u].append(v)
-        
+        for color in range(2):
+            for u,v in redEdges if color == 1 else blueEdges:
+                graph[color][u].append(v)
         def dfs(curr , color , l ):
             ans[color][curr] = min(ans[color][curr] , l)
-            graph = red[curr] if color == 0 else blue[curr]
-            for nei in graph:
+            for nei in graph[color][curr]:
                 if ans[color ^ 1][nei] > l + 1:
                     dfs(nei, color ^ 1, l+1)
-        for x in red[0]:
-            dfs(x,1 , 1)
-        for x in blue[0]:
-            dfs(x,0, 1)
+        for color in range(2):
+            for x in graph[color][0]:
+                dfs(x, color ^ 1 , 1)
         res = [0]
         for c1,c2 in list(zip(*ans))[1:]:
             res.append(min(c1,c2))
