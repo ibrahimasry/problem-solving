1e6dba6fdd55db789f35c6b02fd0afa2f6a55274
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-17 11:43:11 +0200

    Time: 56 ms (99.51%), Space: 13.8 MB (75.18%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -1,11 +1,13 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        y,m,d = date.split("-")
-        days = [31,28 , 31,30,31,30,31,31,30,31,30,31]
-        if y != "1900":
-            days[1] +=  (1 if int(y) % 4 == 0 else 0)
-        res = 0
+        y,m,d = map(int,date.split("-"))
+        over = m-1
+        if int(m) > 7:
+            over += 1
+        over = ceil(over / 2 )
+        overFeb = 2 if m > 2 else 0
+        over30 = over if m > 1 else 0
+        if y != 1900 and m > 2:
+            over30 +=  (1 if int(y) % 4 == 0 else 0)
+        return 30 * (m - 1) + d - overFeb + over30 
         
-        for i in range(int(m)-1):
-            res += days[i]
-        return res + int(d) 
