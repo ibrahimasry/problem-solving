dc9762beb314bb755a6db81ea424e35a5df7cba9
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-20 07:50:04 +0200

    Time: 983 ms (9.23%), Space: 22.5 MB (28.91%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,19 +1,13 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        def sub(i ,curr):
-            nonlocal ans
+        ans = set()
+        n = len(nums)
+        for mask in range(1 << n):
+            curr = []
+            for i in range(n):
+                if mask >> i & 1:
+                    if not curr or curr[-1] <= nums[i]:
+                        curr.append(nums[i])
             if len(curr) > 1:
-                ans.append(curr[:])
-            seen = set()
-            
-            for j in range(i,len(nums)):
-                if  nums[j] in seen:
-                    continue
-                if len(curr) == 0 or curr[-1] <= nums[j]:
-                    seen.add(nums[j])
-                    curr.append(nums[j])
-                    sub(j+1,curr)
-                    curr.pop()
-        sub(0,[])
+                ans.add(tuple(curr))
         return ans
