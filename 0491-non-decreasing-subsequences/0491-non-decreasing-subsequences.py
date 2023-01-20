695e551df64c1302809878268794c769ee94d405
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-20 07:51:17 +0200

    Time: 620 ms (21.26%), Space: 22.5 MB (28.91%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -4,12 +4,14 @@ class Solution:
         n = len(nums)
         for mask in range(1 << n):
             curr = []
+            valid = True
             for i in range(n):
                 if mask >> i & 1:
                     if not curr or curr[-1] <= nums[i]:
                         curr.append(nums[i])
                     else :
+                        valid = False
                         break
-            if len(curr) > 1:
+            if len(curr) > 1 and valid:
                 ans.add(tuple(curr))
         return ans
