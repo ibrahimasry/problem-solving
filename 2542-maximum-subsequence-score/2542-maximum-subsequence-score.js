0533365ec5fd995d37e0a56bb4a5a1e72445a49b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-21 19:26:25 +0200

    Time: 859 ms (100.00%), Space: 101.6 MB (100.00%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.js
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.js
@@ -4,20 +4,14 @@
  * @param {number} k
  * @return {number}
  */
-var maxScore = function(nums1, nums2, k) {
+const maxScore = function(nums1, nums2, k) {
     const  pq = new MinPriorityQueue()
     const n = nums1.length
     nums2 = nums2.map((v,i) => [v,i]).sort((a,b) => a[0]-b[0])
-    let s = 0
-    for (let i = n - 1; i >= 0; i--)
-        if (pq.size() + 1 < k){
-            
+    for (let i = n - 1; i >= 0 && pq.size() + 1 < k; i--)
             pq.enqueue(nums1[nums2[i][1]])
-            s += nums1[nums2[i][1]]
-        }
-        else
-            break
     let j = n-k
+    let s = pq.toArray().map(curr => curr.element).reduce((a,b) => a + b,0)
     let res = -Infinity
     while(j>=0){
         const [v,i] = nums2[j]
