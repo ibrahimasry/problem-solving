1c173afad81e4ebe4042ad05cb56693f94bc88b9
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-22 22:15:18 +0200

    Time: 86 ms (46.53%), Space: 13.8 MB (62.20%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -3,6 +3,6 @@ class Solution:
         m = (n // 2) * 2 + 1
         m -= n % 2 == 0
         res = 0
-        for i in range(n//2 ):
+        for i in range(n >> 1):
             res += m - (i*2 + 1)
         return res
