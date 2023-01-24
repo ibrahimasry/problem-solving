29d498beea24b30cca1751aa71b9ff2d7dc95431
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-25 01:48:08 +0200

    Time: 5826 ms (6.39%), Space: 189.3 MB (19.86%) - LeetHub

--- 1223-dice-roll-simulation/1223-dice-roll-simulation.py
+++ 1223-dice-roll-simulation/1223-dice-roll-simulation.py
@@ -6,13 +6,9 @@ class Solution:
             if j == n:
                 return 1
             ans = 0
-            
             for k in range(6):
                 if k == last and rollMax[last] == count:
                     continue
-                if last == k:
-                    ans += dp(j+1,last, count +1)
-                else :
-                    ans += dp(j+1,k,1)
+                ans += dp(j+1,k,1 if k != last else count + 1)
             return ans % mod
         return dp(0,-1,0)
