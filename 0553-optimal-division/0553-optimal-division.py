6fcf436a774fadf063859d32a920e2d0a8ca2a0e
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-24 21:50:28 +0200

    Time: 29 ms (85.47%), Space: 14 MB (5.13%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.py
+++ 0553-optimal-division/0553-optimal-division.py
@@ -1,9 +1,7 @@
 class Solution:
     def optimalDivision(self, nums: List[int]) -> str:
         nums = [str(n) for n in nums]
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
+        if len(nums) <= 2:
             return "/".join(nums)
         if len(nums) > 1:
             return nums[0] + "/" + "(" + "/".join(nums[1:]) + ")"
