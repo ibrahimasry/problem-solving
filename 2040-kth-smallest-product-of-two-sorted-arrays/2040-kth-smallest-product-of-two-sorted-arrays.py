1cd0d315208dff10a21f39b5c73b551ec5d8da98
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-25 04:42:28 +0200

    Time: 6334 ms (47.50%), Space: 23.9 MB (37.50%) - LeetHub

---
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.py
@@ -0,0 +1,24 @@
+from bisect import bisect, bisect_left
+
+class Solution:
+    def kthSmallestProduct(self, nums1, nums2, k):
+        def check(x):
+            total = 0
+            for n1 in nums1:
+                if n1 > 0: total += bisect(nums2, x//n1)
+                if n1 < 0: total += len(nums2) - bisect_left(nums2, ceil(x/n1))
+                if n1 == 0 and x >= 0: total += len(nums2)
+
+            return total
+
+        beg, end = -10**10 - 1, 10**10 + 1
+
+        while beg < end:
+            mid = (beg + end)//2
+            if check(mid) >= k:
+                end = mid
+            else:
+                beg = mid + 1
+
+        return beg 
+        
