c2a81a3bb410865208fd13152c44fdebb35c9ab0
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-26 01:18:03 +0200

    Time: 1452 ms (83.33%), Space: 54.1 MB (58.33%) - LeetHub

--- 0675-cut-off-trees-for-golf-event/0675-cut-off-trees-for-golf-event.js
+++ 0675-cut-off-trees-for-golf-event/0675-cut-off-trees-for-golf-event.js
@@ -5,40 +5,20 @@
 var cutOffTree = function(forest) {
         let n = forest[0].length
         let m = forest.length
-
-        function bfs(start, target){
-            const q = new Queue();
-            q.push(start)
-            const dist = Array(m).fill(0).map(row=> Array(n).fill(Infinity))
-             dist[start[0]][start[1]] = 0
-            while (q.size() > 0){
-               let [i,j] = q.dequeue()
-                if (forest[i][j] == target)
-                    return dist[i][j]
-                for (const [x,y] of [[i,j-1],[i,j+1],[i-1,j],[i+1,j]])
-                    if (0 <= x && x < m && 0 <= y  && y < n && dist[x][y] == Infinity && forest[x][y] != 0){
-                        q.push([x,y])
-                        dist[x][y] = dist[i][j] + 1
-                        if (target == forest[x][y])
-                            return dist[x][y]
-                    }
-            }
-            return -1
-        }
-        
+        const dirs = (i,j) => [[i,j-1],[i,j+1],[i-1,j],[i+1,j]]
+        const mat = (m,n) => Array(m).fill(0).map(row => Array(n).fill(Infinity))
         const tree = []
-        for(let i = 0; i  < m; i++){
-            for (let j = 0; j < n; j++){
-                if (forest[i][j] > 1)
-                    tree.push([forest[i][j], i, j])
-            }
-        }
+        forest.forEach((row,i) => row.forEach((e,j) => e > 1 && tree.push([e,i,j])))
         tree.sort((a,b)=> b[0] - a[0])
+
+        
         let ans = 0
         let start = [0,0]
-        
         while (tree.length){
             const [target , i , j] = tree.pop()
+            if (start[0] == i && start[1] == j)
+                continue
+            
             const dist = bfs(start,target)
             if (dist == -1)
                 return -1
@@ -46,6 +26,27 @@ var cutOffTree = function(forest) {
             ans += dist
         }
         return ans
+    
+    
+          function bfs(start, target){
+            const q = new Queue();
+            q.push(start)
+            const dist = mat(m,n)
+            dist[start[0]][start[1]] = 0
+            const neis = (i,j) => dirs(i,j).filter(([x,y])=> 0 <= x && x < m && 0 <= y  && y < n && dist[x][y] == Infinity && forest[x][y] != 0)
+
+            while (!q.isEmpty()){
+               let [i,j] = q.dequeue()
+                for (const [x,y] of neis(i,j)){
+                    q.push([x,y])
+                    dist[x][y] = dist[i][j] + 1
+                    if (target == forest[x][y])
+                        return dist[x][y]
+            }
+        }
+            return -1
+     }
+
             
     
 };
