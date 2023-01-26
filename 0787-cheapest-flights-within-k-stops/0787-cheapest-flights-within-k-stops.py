f64c53f4b7c2f6b3ba21a3623e2a374b5439d535
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-26 04:38:19 +0200

    Time: 681 ms (10.31%), Space: 15.1 MB (89.11%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        dist = [inf] * n
+        prev = [inf] * n
+        prev[src] = 0
+        
+        for _ in range(k+1):
+            for u , v , cost in flights:
+                if prev[u] == inf:
+                    continue
+                if prev[u] + cost < dist[v]:
+                    dist[v] = prev[u] + cost
+            prev = dist
+            prev[src] = 0
+            dist = [inf] * n
+        return -1 if prev[dst] == inf else prev[dst]
