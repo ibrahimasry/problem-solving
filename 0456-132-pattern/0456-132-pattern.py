3c8ef91024f4600556edd4d568e286479dd2670b
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-30 00:50:59 +0200

    Time: 376 ms (74.52%), Space: 32.1 MB (68.90%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,15 +1,9 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        minn = nums[:]
-        
-        for i in range(1,len(nums)):
-            minn[i] = min(nums[i], minn[i-1])
+        minn = list(accumulate(nums,min))
         stack = []
-        
         for i in range(len(nums)-1,0,-1):
-            while stack and stack[-1] <= minn[i]:
-                stack.pop()
-            if stack and nums[i] > stack[-1]:
-                return True
+            while stack and stack[-1] <= minn[i]: stack.pop()
+            if stack and nums[i] > stack[-1]: return True
             stack.append(nums[i])
         return False
