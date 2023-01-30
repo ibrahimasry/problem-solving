6ed0f1f9be92e7bb815d37665c48f50849029825
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-30 02:52:24 +0200

    Time: 1456 ms (53.85%), Space: 14.3 MB (69.23%) - LeetHub

--- 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.py
+++ 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.py
@@ -1,22 +1,14 @@
 class Solution:
-    def countQuadruplets(self, a: List[int]) -> int:
-        res, n = 0, len(a)
-        left = [[0 for i in range(n+1)] for j in range(n)]
-        right = [[0 for i in range(n+1)] for j in range(n)]
-        for i in range(1, n):
-            # new array will based on the old array
-            left[i] = left[i-1][:]
-            # update all the elements greater than a[i-1]
-            for j in range(a[i-1] + 1, n + 1):
-                left[i][j] += 1
-        for i in range(n-2, -1, -1):
-            right[i] = right[i+1][:]
-            for j in range(a[i+1]):
-                right[i][j] += 1
-        for j in range(n):
-            for k in range(j+1, n):
-                # left[j][a[k]] means the count of "until j, < a[k]"
-                # right[k][a[j]] means the count of "unitl k, > a[j]"
-                if a[j] > a[k]:
-                    res += left[j][a[k]] * right[k][a[j]]
-        return res
+    def countQuadruplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        ans = 0
+        for i in range(n):
+            prev = 0
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    prev += 1
+                    ans += dp[j]
+                else :
+                    dp[j] += prev
+        return ans
