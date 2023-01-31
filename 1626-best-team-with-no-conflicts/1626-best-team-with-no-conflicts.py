dc3e1783c58e019f54b528c89fd50242149da70f
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-31 05:12:38 +0200

    Time: 158 ms (44.44%), Space: 47.6 MB (55.56%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,12 +1,20 @@
-class Solution:
-    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        
-        pairs = sorted([(v, scores[i]) for i, v in enumerate(ages)])
-        n = len(scores)
-        dp = [-1] * n
-        for i in range(n):
-            dp[i] = pairs[i][1]
-            for j in range(i):
-                if pairs[i][1] >= pairs[j][1]:
-                    dp[i] = max(dp[i] , dp[j] + pairs[i][1])
-        return max(dp)
+/**
+ * @param {number[]} scores
+ * @param {number[]} ages
+ * @return {number}
+ */
+var bestTeamScore = function(scores, ages) {
+    const n = scores.length
+    const dp = Array(n).fill(-1)
+    let pairs = scores.map((v,i) => [ages[i],v]).sort((a,b)=> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0] )
+    pairs = pairs.map(a=>a[1])
+    for(let i = 0; i < n; i++){
+        dp[i] = pairs[i]
+        for(let j=0; j<i; j++){
+            if(pairs[i] >= pairs[j])
+                dp[i] = Math.max(dp[i], dp[j] + pairs[i])
+        }
+    }
+    return Math.max(...dp)
+    
+};
