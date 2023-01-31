37daf30a97b6807ff42bd98299152d901617c7fa
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-01-31 04:44:51 +0200

    Time: 2211 ms (51.07%), Space: 14.2 MB (83.69%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,18 +1,12 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
         
-        cache = [[-1] * 1000 for  _ in range(1000)]
-        def dp(i, prev):
-            if i == n :
-                return 0
-            if cache[i][prev] != -1:
-                return cache[i][prev]
-            res = -1
-            if prev == -1 or pairs[i][1] >= pairs[prev][1]:
-                res = max(res, pairs[i][1] +  dp(i+1, i))
-            res = max(dp(i+1, prev), res)
-            cache[i][prev] = res
-            return res
         pairs = sorted([(v, scores[i]) for i, v in enumerate(ages)])
         n = len(scores)
-        return dp(0,-1)
+        dp = [-1] * n
+        for i in range(n):
+            dp[i] = pairs[i][1]
+            for j in range(i):
+                if pairs[i][1] >= pairs[j][1]:
+                    dp[i] = max(dp[i] , dp[j] + pairs[i][1])
+        return max(dp)
