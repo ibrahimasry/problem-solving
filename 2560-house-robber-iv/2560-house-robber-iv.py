24352d9702c52d7130a7d88dfedd18462210dbbb
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-05 06:27:10 +0200

    Time: 1087 ms (80.00%), Space: 25.1 MB (60.00%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.py
+++ 2560-house-robber-iv/2560-house-robber-iv.py
@@ -5,10 +5,10 @@ class Solution:
             prev = -1
             for num in nums:
                 if prev > 0:
-                    prev = -1
+                    prev *= -1
                 elif num <= target:
                     take += 1
-                    prev = 1
+                    prev *= -1
             return take >= k
         l = 1
         r = max(nums)
