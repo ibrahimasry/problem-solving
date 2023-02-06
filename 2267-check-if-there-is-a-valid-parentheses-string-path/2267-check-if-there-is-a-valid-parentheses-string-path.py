d83c8a91b31fadd095b4f82dd692f7285da86cda
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-06 04:21:40 +0200

    Time: 1824 ms (45.88%), Space: 235.4 MB (21.18%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
@@ -1,21 +1,12 @@
 class Solution:
     def hasValidPath(self, grid: List[List[str]]) -> bool:
-        if grid[0][0] == ')':
-            return False
-        cache = dict()
+        @cache
         def dfs(i, j, opened):
-            nonlocal cache
-            if  i == len(grid)    or j == len(grid[0]) or opened < 0:
+            if  i == len(grid) or j == len(grid[0]) or opened < 0:
                 return False
             if i == len(grid) - 1 and j == len(grid[0]) -1:
-                if opened == 1 and grid[i][j] == ')':
-                    return True
-                return False
-                    
-            if (i,j,opened) in cache:
-                return cache[(i,j,opened)]
+                return opened == 1 and grid[i][j] == ')'
 
             curr = (-1 if grid[i][j] == ")" else 1)
-            cache[(i,j,opened)] = dfs(i+1,j,opened + curr)   or dfs(i,j+1,opened + curr)       
-            return cache[(i,j,opened)]
+            return dfs(i+1,j, opened + curr)   or dfs(i,j+1,opened + curr)       
         return dfs(0,0,0)
