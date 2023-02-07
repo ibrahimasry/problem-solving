6a70331ce72a143304f5fa67317e9e677243caee
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-07 04:47:02 +0200

    Time: 865 ms (88.58%), Space: 20.2 MB (51.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,15 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        prev = -1
-        curr = -1
+        prev = 0
+        curr = 0
         start = 0
         res = 0
         for i in range(len(fruits)):
-            if prev == -1:
-                prev = i
-            elif curr == -1 and fruits[i] != fruits[prev]:
-                curr = i
-            if prev != -1 and curr != -1:
-                if fruits[i] == fruits[prev]:
-                    curr,prev = i,curr
-                elif fruits[i] != fruits[prev] and fruits[i] != fruits[curr]:
-                    curr,prev = i, curr
-                    start = prev
+            if fruits[i] == fruits[prev] or fruits[prev] == fruits[curr]:
+                curr,prev = i,curr
+            elif fruits[i] != fruits[prev] and fruits[i] != fruits[curr]:
+                curr,prev = i, curr
+                start = prev
             res = max((i - start) + 1, res)
         return res
                 
