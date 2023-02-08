1b082b6bd640a60e87303176358e9c2df6b4b456
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-08 05:26:55 +0200

    Time: 135 ms (71.14%), Space: 14.9 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -7,11 +7,12 @@ class Solution:
         dist = len(nums) - 1
         for i, j in enumerate(nums[:-1]):
             mxW = max(mxW,i+j)
-            if mxW >= dist:
-                return res + 1
 
             if i >= curr:
                 curr = mxW
                 res += 1
+            if curr >= dist:
+                return res
+
         return res
         
