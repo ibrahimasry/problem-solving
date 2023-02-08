7580d4c330f7c0293e1e2a4ce714e0af881e39c5
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-08 05:31:50 +0200

    Time: 131 ms (80.19%), Space: 14.9 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         res = 0
-        mxW  = -1
-        curr = -1
+        nextWindow  = -1
+        currWindow = -1
         for i, j in enumerate(nums[:-1]):
-            mxW = max(mxW,i+j)
-
-            if i >= curr:
-                curr = mxW
+            nextWindow = max(nextWindow,i+j)
+            if i >= currWindow:
+                currWindow = nextWindow
                 res += 1
 
         return res
