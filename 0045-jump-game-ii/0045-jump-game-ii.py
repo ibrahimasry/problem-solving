c63336fe317d4eb847dc8304f777cf2e76f5b012
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-08 05:27:48 +0200

    Time: 126 ms (90.62%), Space: 15 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,11 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
         res = 0
         
         mxW  = -1
         curr = -1
         dist = len(nums) - 1
-        for i, j in enumerate(nums[:-1]):
+        for i, j in enumerate(nums):
             mxW = max(mxW,i+j)
 
             if i >= curr:
