f3cf8dc2657dde040c28034bc59b085054e19e2a
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-08 08:46:08 +0200

    Time: 6745 ms (33.32%), Space: 14 MB (38.10%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -2,34 +2,17 @@ class Solution:
     def largestVariance(self, s: str) -> int:
         if len(set(s)) == 1 or len(set(s)) == len(s):
             return 0
-        seen = set(s)
         res = 0
-        chrToIdx = defaultdict(list)
-        for i , c in enumerate(s):
-            chrToIdx[c].append(i)
         counts = Counter(s)
-        for c1 in chrToIdx:
-            for c2 in chrToIdx:
-                if c1 == c2:
+        for c1 in counts:
+            for c2 in counts:
+                if c1 == c2 :
                     continue
-                arr = []
-                i = j = 0
-                first = chrToIdx[c1]
-                second = chrToIdx[c2]
-                while i < len(first) and j < len(second):
-                    if first[i] < second[j]:
-                        arr.append(first[i])
-                        i += 1
-                    else :
-                        arr.append(second[j])
-                        j += 1
-                arr += first[i:] + second[j:]
                 curr = 0
                 total = 0
                 small = 0
                 currSmall = 0
-                for i in arr:
-                    c = s[i]
+                for c in s:
                     if c == c1:
                         curr -= 1
                     elif c == c2:
@@ -44,7 +27,7 @@ class Solution:
                             curr = 0
                             currSmall = 0
                     if currSmall > 0:
-                        total = max(total , curr)
-                res = max(res, total)
+                        if curr > res:
+                            res = curr
                     
         return res
