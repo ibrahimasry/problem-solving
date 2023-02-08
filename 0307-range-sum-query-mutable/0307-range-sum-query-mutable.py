c3c75d096edd86032f72bce023023a80fbcfc741
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-08 03:00:50 +0200

    Time: 2806 ms (57.08%), Space: 32.6 MB (37.52%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,58 +1,55 @@
-class Node:
-    def __init__(self, start,end):
-        self.start = start
-        self.end = end
-        self.total = 0
-        self.left = None
-        self.right = None
 
 class NumArray:
     def __init__(self, nums: List[int]):
-        def buildTree(start,end):
+        def buildTree(index, start,end):
+            tree=self.tree
             if start > end:
                 return None
             if start == end:
-                root = Node(start,end)
-                root.total = nums[start]
-                return root
+                tree[index] = nums[start]
+                return
+            tree = self.tree
             m = (start + end) // 2
-            root  = Node(start,end)
-            root.left  =  buildTree(start, m)
-            root.right = buildTree(m+1,end)
-            root.total = root.left.total + root.right.total
-            return root
-        self.tree = buildTree(0,len(nums)-1)
+            buildTree(index*2  ,start, m)
+            buildTree(index*2+1,m+1,end)
+            tree[index] = tree[index*2] + tree[index*2 + 1]
+        self.tree = [0] * (len(nums) * 4) 
+        self.l = len(nums)
+        buildTree(1,0,len(nums)-1)
 
         
 
     def update(self, pos: int, val: int) -> None:
-            def helper(root, pos, val):
-                if root.end == root.start:
-                    root.total = val
-                    return val
-                m = (root.start + root.end) // 2
+            def helper(index, start,end, pos, val):
+                tree= self.tree
+                if start == end:
+                    tree[index] = val
+                    return
+                m = (start + end) // 2
                 if pos <= m:
-                    helper(root.left, pos,val)
+                    helper(index*2, start,m, pos,val)
                 else:
-                    helper(root.right,pos,val)
-                root.total = root.left.total + root.right.total
-                return root.total
-            return helper(self.tree,pos,val)
+                    helper(index*2+1 , m+1, end, pos , val)
+                tree[index] = tree[index*2] + tree[index*2+1]
+            return helper(1, 0,self.l - 1 ,pos,val)
 
         
 
     def sumRange(self, left: int, right: int) -> int:
-        def helper(root, start, end):
-            if root.end == end and root.start==start:
-                return root.total
-            m = (root.start + root.end) // 2
+        def helper(index,l , r , start, end):
+            tree = self.tree
+            if start > r or end < l:
+                return 0
+            if start <= l and end >= r:
+                return tree[index]
+            m = (l+r) // 2
             if start >= m + 1:
-                return helper(root.right, start,end)
+                return helper(index*2+1 ,m+1,r, start,end)
             elif end <= m:
-                return helper(root.left,start, end)
+                return helper(index*2,l,m,start, end)
             else:
-                return helper(root.left,start,m) + helper(root.right, m+1,end)
-        return helper(self.tree,left,right)
+                return helper(index*2,l,m,start, end) + helper(index*2+1 ,m+1,r, start,end)
+        return helper(1,0,self.l-1,left,right)
 
         
 
