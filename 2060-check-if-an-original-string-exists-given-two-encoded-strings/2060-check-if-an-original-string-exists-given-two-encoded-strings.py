33459d187ae32504dc6d4763bc10e00583b41a5d
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-09 09:43:11 +0200

    Time: 1063 ms (60.53%), Space: 116.7 MB (23.68%) - LeetHub

--- 2060-check-if-an-original-string-exists-given-two-encoded-strings/2060-check-if-an-original-string-exists-given-two-encoded-strings.py
+++ 2060-check-if-an-original-string-exists-given-two-encoded-strings/2060-check-if-an-original-string-exists-given-two-encoded-strings.py
@@ -7,21 +7,15 @@ class Solution:
             if i < len(s1) and s1[i].isdigit():
                 start = i
                 while i < len(s1) and s1[i].isdigit():
-                    if dp(i+1,j,diff + int(s1[start:i+1])):
-                        return True
+                    if dp(i+1,j,diff + int(s1[start:i+1])): return True
                     i += 1
             elif  j < len(s2) and s2[j].isdigit():
                 start = j
                 while j < len(s2) and s2[j].isdigit():
-                    if dp(i,j+1,diff-int(s2[start:j+1])):
-                        return True
+                    if dp(i,j+1,diff-int(s2[start:j+1])): return True
                     j += 1
-            elif i < len(s1) and diff < 0 :
-                if dp(i+1, j ,diff + 1):
-                    return True
-            elif j < len(s2) and diff > 0 :
-                if dp(i,j+1,diff-1):
-                    return True
+            elif i < len(s1) and diff < 0 and dp(i+1 ,j ,diff + 1): return True
+            elif j < len(s2) and diff > 0 and dp(i ,j+1 ,diff-1) : return True
             elif i < len(s1) and j < len(s2) and s1[i] == s2[j] and diff == 0:
                 if dp(i+1,j+1,diff):
                     return True
