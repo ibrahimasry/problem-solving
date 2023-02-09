9039a88dfc7b7059d800b6530003096d49c548ea
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-09 09:29:34 +0200

    Time: 1365 ms (48.68%), Space: 127.3 MB (10.53%) - LeetHub

---
+++ 2060-check-if-an-original-string-exists-given-two-encoded-strings/2060-check-if-an-original-string-exists-given-two-encoded-strings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def possiblyEquals(self, s1: str, s2: str) -> bool:
+        @cache
+        def dp(i,j,diff):
+            if i == len(s1) and j == len(s2):
+                return diff == 0
+            start = i
+            while i < len(s1) and s1[i].isdigit():
+                if dp(i+1,j,diff + int(s1[start:i+1])):
+                    return True
+                i += 1
+            i = start
+            start = j
+            while j < len(s2) and s2[j].isdigit():
+                if dp(i,j+1,diff-int(s2[start:j+1])):
+                    return True
+                j += 1
+            j = start
+            if i < len(s1) and diff < 0 and s1[i].isalpha():
+                if dp(i+1, j ,diff + 1):
+                    return True
+            elif j < len(s2) and diff > 0 and s2[j].isalpha():
+                if dp(i,j+1,diff-1):
+                    return True
+            elif i < len(s1) and j < len(s2) and s1[i] == s2[j] and diff == 0:
+                if dp(i+1,j+1,diff):
+                    return True
+            return False
+        return dp(0,0,0)
+            
