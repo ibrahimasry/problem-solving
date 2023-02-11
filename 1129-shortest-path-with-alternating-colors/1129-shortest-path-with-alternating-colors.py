a72b4c7853087d09d750689773ba5a8fb62a0f91
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-11 08:45:59 +0200

    Time: 104 ms (31.96%), Space: 14.2 MB (73.17%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -3,17 +3,24 @@ class Solution:
         
         graph = [defaultdict(list) for _ in range(2)]
         ans = [[inf] * n for _ in range(2)]
+        
         for color in range(2):
             for u,v in redEdges if color == 1 else blueEdges:
                 graph[color][u].append(v)
+        
+        
         def dfs(curr , color , l ):
             ans[color][curr] = min(ans[color][curr] , l)
             for nei in graph[color][curr]:
                 if ans[color ^ 1][nei] > l + 1:
                     dfs(nei, color ^ 1, l+1)
+        
+        
         for color in range(2):
             for x in graph[color][0]:
                 dfs(x, color ^ 1 , 1)
+        
+        
         res = [0]
         for c1,c2 in list(zip(*ans))[1:]:
             res.append(min(c1,c2))
