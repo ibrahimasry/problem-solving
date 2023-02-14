414a355695ffdaa3f84fd5d8d5a5731c25a81075
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-14 09:21:28 +0200

    Time: 675 ms (78.95%), Space: 138.4 MB (28.01%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -8,22 +8,25 @@ class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
         path1 = []
         path2 = []
-        
         def dfs(node):
             if not node:
                 return 0
             left = dfs(node.left)
             right = dfs(node.right)
-            if left == 1:
-                path2.append('L')
-            if right == 1:
-                path2.append('R')
+            #found the dist so we get down to it by left and right
+            if left == 1 or right == 1:
+                path2.append('L' if left == 1 else "R")
+                
+            #found the start so we go up till we find the lca
             if left == 2 or right == 2:
                 path1.append('U')
+            #we find the lca then reset and dont add any dirs
             if left + right == 3:
-                return 4
+                return 0
+            #we find the start add 2 as a sign we found the start
             if node.val == startValue:
                 return 2 + left + right
+            #we find the dist add 1 as a sign we found the start
             if node.val == destValue:
                 return 1 + left + right
 
