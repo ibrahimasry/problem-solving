f17b6ae3c7033779ac39562a1101a2b600ef5981
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-16 10:40:50 +0200

    Time: 2527 ms (81.79%), Space: 15.1 MB (74.69%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -1,16 +1,27 @@
 from sortedcontainers import SortedSet
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
+        s = sum(nums) 
+        half = sum(nums) // 2
         n = len(nums)//2
-        left, right = nums[:n], nums[n:]
-        lsum, rsum = sum(left), sum(right)
         
-        ans = inf
-        for i in range(n+1): 
-            vals = sorted(2*sum(combo)-lsum for combo in combinations(left, i))
-            for combo in combinations(right, n-i): 
-                diff = 2*sum(combo) - rsum
-                k = bisect_left(vals, -diff)
-                if k: ans = min(ans, abs(vals[k-1] + diff))
-                if k < len(vals): ans = min(ans, abs(vals[k] + diff))
-        return ans 
+        res = inf
+        for i in range(n+1):
+            left = [sum(com) for com in combinations(nums[:n],i)]
+            right = sorted([sum(com) for com in combinations(nums[n:], n - i)])
+            for a in left:
+                b = half - a
+                curr = bisect.bisect_left(right,b)
+                if curr > 0:
+                    cand2 = right[curr-1]
+                    l = cand2 + a
+                    r = s - l
+                    if abs(l-r) < res:
+                        res = abs(l-r)
+                if curr < len(right):
+                    cand2 = right[curr]
+                    l = cand2 + a
+                    r = s - l
+                    if abs(l-r) < res:
+                        res = abs(l-r)
+        return res
