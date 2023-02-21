403a8bd3eb0f0bea3f7b8f49a8f0efb6cee0ebf5
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-21 17:31:03 +0200

    Time: 154 ms (93.90%), Space: 14.1 MB (35.37%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
@@ -3,13 +3,11 @@ class Solution:
         def go(nums1,nums2):
             count2 = Counter([n**2 for n in nums2])
             cnt1 = (Counter(nums1))
-            keys1 = sorted(list(set(nums1)))
+            keys1 = list(set(nums1))
             res = 0
             for i in range(len(keys1)):
-                for j in range(i,len(keys1)):
-                    if i != j  :
-                        res += count2[keys1[i] * keys1[j]] * cnt1[keys1[i]] * cnt1[keys1[j]]
-                    if i == j  :
-                        res += count2[keys1[i] * keys1[j]] * (cnt1[keys1[i]]-1) * (cnt1[keys1[i]]) // 2
+                res += count2[keys1[i] ** 2] * (cnt1[keys1[i]]-1) * (cnt1[keys1[i]]) // 2
+                for j in range(i+1,len(keys1)):
+                    res += count2[keys1[i] * keys1[j]] * cnt1[keys1[i]] * cnt1[keys1[j]]
             return res
         return go(nums1,nums2) + go(nums2,nums1)
