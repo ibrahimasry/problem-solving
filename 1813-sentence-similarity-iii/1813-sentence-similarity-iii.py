ba302bc77ca10a4bf6a71890e317624a26042659
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-02-21 17:46:44 +0200

    Time: 45 ms (9.46%), Space: 13.9 MB (18.92%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -1,13 +1,11 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
-        if sentence1 == sentence2:
-            return True
-        if len(sentence1) < len(sentence2):
-            sentence1, sentence2 = sentence2,sentence1
-        sentence1 = sentence1.split(' ')
-        sentence2 = sentence2.split(' ')
-        for i in range(len(sentence1)):
-            for j in range(i,len(sentence1)+1):
-                if sentence1[:i] + sentence1[j:]== sentence2:
-                    return True
-        return False
+        deq1,deq2 = map(deque, (sentence1.split(), sentence2.split()))
+        while deq1 and deq2 and deq1[0] == deq2[0]:
+            deq1.popleft()
+            deq2.popleft()
+        while deq1 and deq2 and deq1[-1] == deq2[-1]:
+            deq1.pop()
+            deq2.pop()
+
+        return not deq1 or not deq2
