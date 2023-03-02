20c3574d49ea2b4f22c602126b41fb9392997f18
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-03-02 13:08:34 +0200

    Time: 60 ms (73.48%), Space: 14.1 MB (25.81%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -7,8 +7,8 @@ class Solution:
                 l = len(str(i - r + 1))
                 chars[left] = chars[r]
                 left += 1
-                if i - r + 1 > 1:
-                    chars[left:left+l] = [c for c in str(i - r + 1)]
+                if i > r:
+                    chars[left:left+l] = list(str(i - r + 1))
                     left += l
                 r = i + 1
         del chars[left:]
