8bf00e6ab257a092bdd03dfa7949f0bf90d77f55
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-03-02 13:03:32 +0200

    Time: 64 ms (51.18%), Space: 13.9 MB (65.01%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -2,28 +2,14 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         r = 0
         left = 0
-        for i in range(1,len(chars)):
-            if chars[i] != chars[r]:
-                if i - r == 1:
-                    chars[left] = chars[r]
-                    left += 1
-
-                    r = i
-                    
-                else:
-                    l = len(str(i - r))
-                    chars[left] = chars[r]
-                    left += 1
-                    chars[left:left+l] = [c for c in str(i - r)]
+        for i in range(len(chars)):
+            if i == (len(chars) - 1) or chars[i] != chars[i+1]:
+                l = len(str(i - r + 1))
+                chars[left] = chars[r]
+                left += 1
+                if i - r + 1 > 1:
+                    chars[left:left+l] = [c for c in str(i - r + 1)]
                     left += l
-                    r = i
-        if len(chars) - r > 1:
-            l = len(str(len(chars) - r))
-            chars[left] = chars[r]
-
-            chars[left+1:left+1+l] = [c for c in str(len(chars) - r)]
-            del chars[left+1+l:]
-        else :
-            chars[left] = chars[r]
-            del chars[left+1:]
+                r = i + 1
+        del chars[left:]
         
