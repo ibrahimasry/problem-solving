b2f00d491aa5512f6cf4315726b964e14bd0188d
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-03-26 11:35:10 +0200

    Time: 288 ms (78.57%), Space: 14.2 MB (7.14%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        primes = [True] * 1001  * 4
+        primes[0] = False
+        primes[1] = False
+        for i in range(2,1001):
+            
+            if primes[i]:
+                for j in range(i+i, 1001, i):
+                    primes[j] = False
+        arr = [nums[-1]]
+        primes = [i for i, n in enumerate(primes) if n]
+        for n in nums[::-1][1::]:
+            if n >= arr[-1]:
+                index = bisect_left(primes,  (n-arr[-1])+1)
+                print(n - primes[index])
+                if  n - primes[index] >= arr[-1] or n <= primes[index]:
+                    return False
+                arr.append(n - primes[index])
+            else :
+                arr.append(n)
+        return True
