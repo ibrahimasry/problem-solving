67472887048a16a9cd3dbabbf253bdd71ced75ef
ibrahimasry <ibrahim.elmansoury@gmail.com>
2023-04-09 00:47:05 +0200

    Time: 2085 ms (97.87%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -4,8 +4,8 @@
 select s.name as name from salesperson s where 
 s.sales_id not in (
 select o.sales_id from orders as o
- join company as c on o.com_id = c.com_id 
-where c.name = "RED"
+ join company as c on o.com_id = c.com_id and c.name = "RED"
+
 
 ) 
 
